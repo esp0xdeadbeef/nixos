e55f5bd6dead050e78edcf89d7745dc801a5133e
esp0xdeadbeef <esp0xdeadbeef@gmail.com>
2025-03-16 00:31:08 +0000

    Added auto updater to github

--- system/autoupdate.nix
+++ system/autoupdate.nix
@@ -5,10 +5,10 @@
  #############################
   system.autoUpgrade = {
     enable = true;
-    flake = "/etc/nixos"; # Explicitly set the flake path
+    flake = "github:esp0xdeadbeef/nixos#l-werk"; # Explicitly set the flake path
     flags = [
       "--impure"
-      "--flake" "/etc/nixos#nixos"
+      "--flake" # "/etc/nixos#nixos"
     ];
     dates = "02:00";
     randomizedDelaySec = "45min";
