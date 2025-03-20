1dcb4d9c4cd4139dfcb7b9a87949973cb8b2ee64
esp0xdeadbeef <esp0xdeadbeef@gmail.com>
2025-03-20 12:08:37 +0000

    added burp shit (suite) softlinks in /etc/burp/
    Which are:
    drwxr-xr-x  2 root root 4096 20 mrt 11:44 .
    drwxr-xr-x 43 root root 4096 20 mrt 11:44 ..
    lrwxrwxrwx  1 root root   26 20 mrt 11:44 jruby.jar -> /etc/static/burp/jruby.jar
    lrwxrwxrwx  1 root root   27 20 mrt 11:44 jython.jar -> /etc/static/burp/jython.jar

--- desktop/packages.nix
+++ desktop/packages.nix
@@ -8,6 +8,19 @@
               #services.openssh.enable = true;
               #services.openssh.settings.X11Forwarding = true;
 
+# creating a a softlink for burp shit (suite)
+environment.etc."burp/jython.jar" = {
+    # If you have a jython package in your Nixpkgs, you can use it like:
+    source = "${pkgs.jython}/jython.jar";
+    # Alternatively, if you want to hardcode the current store path, you can:
+    # source = "/nix/store/akb86svs9qd561a5l27252pqyd8dyds4-jython-2.7.4/jython.jar";
+  };
+
+  environment.etc."burp/jruby.jar" = {
+    source = "${pkgs.jruby}/lib/jruby.jar";
+  };
+
+
               programs.wireshark.enable = true;
 services.locate = {
              enable = true;
@@ -37,6 +50,8 @@ navi
                 nmap
 		autorandr
 gh
+sshpass
+man-pages
 sbctl
 openssl
 tpm2-tools
