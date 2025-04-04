6fbd5abc1282398c122a1f2eb3098412726aab13
esp0xdeadbeef <esp0xdeadbeef@gmail.com>
2025-04-04 16:01:53 +0000

    added flake.nix

--- flake.nix
+++ flake.nix
@@ -81,9 +81,9 @@
           #./hardware/l-esp/hardware-configuration.nix
           {
              networking.hostName = "s-esp";
-             boot.loader.systemd-boot.enable = true;
-             boot.loader.efi.canTouchEfiVariables = true;
-             nixpkgs.config.allowUnfree = true;
+             #boot.loader.systemd-boot.enable = true;
+             #boot.loader.efi.canTouchEfiVariables = true;
+             #nixpkgs.config.allowUnfree = true;
           }
         ] true;
         s-router-vpn-1 = mkNixOS "s-router-vpn-1" [
