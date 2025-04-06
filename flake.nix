7a5be3eae648dff9a7b835da582dfb8766023950
esp deadbeef <esp0xdeadbeef@gmail.com>
2025-04-07 00:29:19 +0200

    added it as a hardware-configuration.nix file instead of hardcoding.

--- flake.nix
+++ flake.nix
@@ -157,23 +157,7 @@
           system = "aarch64-linux";
           modules = [
             nixos-x13s.nixosModules.default
-            ({ config, pkgs, ... }: {
-              networking.hostName = "l-x13s";
-              nixos-x13s.enable = true;
-              nixos-x13s.bluetoothMac = "E9:1C:3B:F0:FD:8C";
-              nixos-x13s.wifiMac = "8c:fd:f0:1c:3b:0a";
-              specialisation = {
-                mainline.configuration.nixos-x13s.kernel = "mainline";
-              };
-              nixpkgs.config.allowUnfree = true;
-              boot.initrd.luks.devices = {
-                root = { device = "/dev/nvme0n1p2"; };
-              };
-              fileSystems."/".device = "/dev/mapper/root";
-              system.stateVersion = "24.11";
-              services.openssh.enable = true;
-              environment.systemPackages = with pkgs; [ util-linux ];
-            })
+            ./hardware/l-x13s/hardware-configuration.nix
           ];
         };
       };
