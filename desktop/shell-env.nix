eaead6009b25e7931be1929491f6002b2427213b
esp deadbeef <esp0xdeadbeef@gmail.com>
2025-04-07 02:12:53 +0200

    added widevine but doesn't work and other random shit.

--- desktop/shell-env.nix
+++ desktop/shell-env.nix
@@ -5,8 +5,13 @@
     if [ -f ~/.aliases ]; then
       source ~/.aliases
     fi
-
+    (
+    cd $(env | grep '^ZSH=.*-oh-my-zsh-.*' | cut -d '=' -f 2)
+    source ./oh-my-zsh.sh 2>/dev/null
+    )
   '';
+  
+
   users.defaultUserShell = pkgs.zsh;
 
   programs.zsh = {
---
+++ packages/l-x13s/widevine.nix
@@ -0,0 +1,7 @@
+{ config, nixpkgs, pkgs, nixos-aarch64-widevine, ... }:
+{
+              # nixpkgs.overlays = [ nixos-aarch64-widevine.overlays.default ];
+              environment.sessionVariables = {
+                MOZ_GMP_PATH = "${pkgs.widevine-cdm-lacros}/gmp-widevinecdm/system-installed";
+              };
+}
