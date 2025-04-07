94ad3f9cb755a586065f46ca68f03f307d2ac542
esp deadbeef <esp0xdeadbeef@gmail.com>
2025-04-07 02:17:02 +0200

    widevine actually works.

--- packages/l-x13s/widevine.nix
+++ packages/l-x13s/widevine.nix
@@ -1,7 +1,13 @@
-{ config, nixpkgs, pkgs, nixos-aarch64-widevine, ... }:
 {
-              # nixpkgs.overlays = [ nixos-aarch64-widevine.overlays.default ];
-              environment.sessionVariables = {
-                MOZ_GMP_PATH = "${pkgs.widevine-cdm-lacros}/gmp-widevinecdm/system-installed";
-              };
+  config,
+  nixpkgs,
+  pkgs,
+  nixos-aarch64-widevine,
+  ...
+}:
+{
+  # nixpkgs.overlays = [ nixos-aarch64-widevine.overlays.default ];
+  environment.sessionVariables = {
+    MOZ_GMP_PATH = "${pkgs.widevine-cdm-lacros}/gmp-widevinecdm/system-installed";
+  };
 }
