3e544268df2de6bec406b2697033e27c41dc0e5f
esp0xdeadbeef <esp0xdeadbeef@l-x13s.lan>
2025-04-08 21:42:25 +0200

    added boot shit to the hardware configuration.

--- hardware/s-router-vpn-1/ssh-vim-and-basics.nix
+++ hardware/s-router-vpn-1/ssh-vim-and-basics.nix
@@ -1,7 +1,5 @@
 { config, pkgs, ... }:
 {
-  boot.loader.grub.enable = true;
-  boot.loader.grub.device = "/dev/sda";
   services.openssh.enable = true;
   environment.systemPackages = with pkgs; [
     vim
