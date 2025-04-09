2294c5260f7dce90fbd86b7c2fe1baa56508f408
esp0xdeadbeef <esp0xdeadbeef@l-x13s.lan>
2025-04-10 01:32:13 +0200

    moved it to the swap-and-tmp config

--- hardware/l-werk/hardware-configuration.nix
+++ hardware/l-werk/hardware-configuration.nix
@@ -48,16 +48,6 @@
   };
   boot.initrd.luks.devices."root".device = "/dev/disk/by-uuid/be6d5b2f-d108-4535-9646-249bd6ac8630";
 
-  swapDevices = [
-    {
-      device = "/var/lib/swapfile";
-      size = 45 * 1024;
-    }
-  ];
-  fileSystems."/tmp" = {
-    device = "tmpfs";
-    fsType = "tmpfs";
-  };
 
   # Enables DHCP on each ethernet and wireless interface. In case of scripted networking
   # (the default) this is the recommended approach. When using systemd-networkd it's
