52ae5739416cf94767f9c756a7c69d3e2c14a0cf
esp0xdeadbeef <esp0xdeadbeef@l-x13s.lan>
2025-04-09 23:42:52 +0200

    lots of changes in network configuration, not dieing now (can still ssh to the box) and vlans

--- network/router/management-network.nix
+++ network/router/management-network.nix
@@ -5,66 +5,10 @@
   ...
 }:
 
-let
-  vlanRange = lib.range 1 10;
-  # Build a list of tuples: [ { name = "vlan001"; id = 1; } ... ]
-  vlanData = map (n: {
-    name = "vlan" + (lib.fixedWidthString 3 "0" (toString n));
-    id = n;
-  }) vlanRange;
-
-  vlanList = lib.listToAttrs (
-    map (v: {
-      name = v.name;
-      value = {
-        id = v.id;
-        interface = "br0";
-      };
-    }) vlanData
-  );
-
-  generatedInterfaces = lib.listToAttrs (
-    map (v: {
-      name = v.name;
-      value = {
-        useDHCP = true;
-      };
-    }) vlanData
-  );
-
-in
 {
-  networking = {
-    useNetworkd = true;
-    useDHCP = false;
+  # nmcli connection modify Wired\ connection\ 1 ipv4.route-metric 99999 ipv6.route-metric 50
+  networking.networkmanager.enable = true;
 
-    vlans = vlanList;
-
-    bridges = {
-      "br0" = {
-        interfaces = [ "enp0s19" ];
-      };
-    };
-
-    interfaces = generatedInterfaces // {
-      enp0s18.useDHCP = true;
-      br0.useDHCP = false;
-
-      # manual overide:
-      # vlan007 = {
-      #   ipv4.addresses = [
-      #     {
-      #       address = "10.0.5.5";
-      #       prefixLength = 24;
-      #     }
-      #   ];
-      # };
-    };
-
-    hostId = lib.mkDefault (
-      builtins.substring 0 8 (builtins.hashString "md5" config.networking.hostName)
-    );
-
-    firewall.enable = false;
-  };
+  # Let NM manage ens18 (management NIC)
+  networking.networkmanager.unmanaged = [ "phys0" ];
 }
