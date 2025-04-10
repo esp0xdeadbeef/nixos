2f32636dfbffa03145430f513a9b2925fc37b95a
esp0xdeadbeef <esp0xdeadbeef@l-x13s.lan>
2025-04-10 21:25:59 +0200

    added a failing impermanence file, it's compiling so i let it for now

---
+++ modules/s-test-vm/impermanence.nix
@@ -0,0 +1,45 @@
+{
+  config,
+  lib,
+  username,
+  ...
+}:
+
+let
+  homeDir = "/home/${username}";
+in
+{
+  boot.initrd.persistentStorage.enable = true;
+
+  fileSystems."/" = lib.mkForce {
+    device = "none";
+    fsType = "tmpfs";
+    options = [
+      "defaults"
+      "mode=755"
+      "size=4G"
+    ];
+  };
+
+
+  fileSystems."/persist" = {
+    # select your root partition and add a label with:
+    # e2label /dev/mapper/luks-b5d59382-e651-4a67-b490-730f085e9706
+    # PERSIST
+    device = "/dev/disk/by-label/PERSIST";
+    fsType = "ext4";
+    neededForBoot = true;
+  };
+
+
+  environment.persistence."/persist".directories = [
+    homeDir
+    "/root"
+    "/etc/ssh"
+    "/var/lib/nixos"
+  ];
+
+  environment.persistence."/persist".files = [
+    "/etc/machine-id"
+  ];
+}
