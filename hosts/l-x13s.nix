60c14f88d6e0b57e582fab662c23ca4ff19b32eb
esp0xdeadbeef <esp0xdeadbeef@l-x13s.lan>
2025-04-11 19:19:29 +0200

    formatted

--- hosts/l-x13s.nix
+++ hosts/l-x13s.nix
@@ -1,42 +1,49 @@
-{ mkNixOS, nixos-x13s, nixos-aarch64-widevine, home-manager, nixpkgs }:
+{
+  mkNixOS,
+  nixos-x13s,
+  nixos-aarch64-widevine,
+  home-manager,
+  nixpkgs,
+}:
 
 {
-  l-x13s = mkNixOS "aarch64-linux" "l-x13s"
-    [
-      nixos-x13s.nixosModules.default
-      ./hardware/l-x13s/hardware-configuration.nix
-    ]
-    [
-      home-manager.nixosModules.home-manager
-      ./home-manager/l-x13s/home.nix
-      ./time/timezone.nix
-      {
-        networking.hostName = "l-x13s";
-        networking.networkmanager.enable = true;
-        nixpkgs.config.allowUnfree = true;
-        boot.loader.systemd-boot.configurationLimit = 15;
-        nixpkgs.overlays = [ nixos-aarch64-widevine.overlays.default ];
-      }
-      ./packages/l-x13s/widevine.nix
-      ./desktop/fonts.nix
-      ./desktop/environment.nix
-      ./system/garbage-collection.nix
-      ./system/locale.nix
-      ./network/firewall.nix
-      ./desktop/applets.nix
-      ./desktop/darkmode.nix
-      ./desktop/shell-env.nix
-      ./desktop/users-and-groups.nix
-      ./system/version.nix
-      ./system/autoupdate.nix
-      ./packages/l-x13s/packages.nix
-      ./general/tooling.nix
-      {
-        environment.interactiveShellInit = ''
-          ZSH_THEME=trapd00r
-        '';
-      }
-    ]
-    true
-    false;
+  l-x13s =
+    mkNixOS "aarch64-linux" "l-x13s"
+      [
+        nixos-x13s.nixosModules.default
+        ./hardware/l-x13s/hardware-configuration.nix
+      ]
+      [
+        home-manager.nixosModules.home-manager
+        ./home-manager/l-x13s/home.nix
+        ./time/timezone.nix
+        {
+          networking.hostName = "l-x13s";
+          networking.networkmanager.enable = true;
+          nixpkgs.config.allowUnfree = true;
+          boot.loader.systemd-boot.configurationLimit = 15;
+          nixpkgs.overlays = [ nixos-aarch64-widevine.overlays.default ];
+        }
+        ./packages/l-x13s/widevine.nix
+        ./desktop/fonts.nix
+        ./desktop/environment.nix
+        ./system/garbage-collection.nix
+        ./system/locale.nix
+        ./network/firewall.nix
+        ./desktop/applets.nix
+        ./desktop/darkmode.nix
+        ./desktop/shell-env.nix
+        ./desktop/users-and-groups.nix
+        ./system/version.nix
+        ./system/autoupdate.nix
+        ./packages/l-x13s/packages.nix
+        ./general/tooling.nix
+        {
+          environment.interactiveShellInit = ''
+            ZSH_THEME=trapd00r
+          '';
+        }
+      ]
+      true
+      false;
 }
