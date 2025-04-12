118f112bd5034e1764d09f081f7c82f390fa5848
esp0xdeadbeef <esp0xdeadbeef@l-x13s.lan>
2025-04-12 18:43:07 +0200

    added l-werk, streamlined the l-x13s

---
+++ nixos/l-werk/hardware/sound-fix-l-werk.nix
@@ -0,0 +1,23 @@
+{ config, pkgs, ... }:
+
+{
+  # Ensure the environment has the necessary package
+  environment.systemPackages = with pkgs; [
+    alsa-utils
+  ];
+
+  # Define a systemd service to run alsactl init on every reboot
+  systemd.services.alsaInit = {
+    wantedBy = [ "multi-user.target" ];
+    after = [ "sound.target" ];
+    serviceConfig = {
+      Type = "oneshot";
+      RemainAfterExit = true;
+      ExecStart = [
+        ''
+          /bin/sh -c '${pkgs.alsa-utils}/bin/alsactl init ; echo error overwrite '
+        ''
+      ];
+    };
+  };
+}
