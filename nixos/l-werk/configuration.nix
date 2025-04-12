be8a3fa8eebe892890fedb8476d11bd02d72e7be
esp0xdeadbeef <esp0xdeadbeef@l-x13s.lan>
2025-04-13 00:42:25 +0200

    disabled ssh on work laptop

--- nixos/l-werk/configuration.nix
+++ nixos/l-werk/configuration.nix
@@ -135,16 +135,16 @@
   };
   # This setups a SSH server. Very important if you're setting up a headless system.
   # Feel free to remove if you don't need it.
-  services.openssh = {
-    enable = false;
-    settings = {
-      # Opinionated: forbid root login through SSH.
-      PermitRootLogin = "no";
-      # Opinionated: use keys only.
-      # Remove if you want to SSH using passwords
-      PasswordAuthentication = true;
-    };
-  };
+  # services.openssh = {
+  #   enable = false;
+  #   settings = {
+  #     # Opinionated: forbid root login through SSH.
+  #     PermitRootLogin = "no";
+  #     # Opinionated: use keys only.
+  #     # Remove if you want to SSH using passwords
+  #     PasswordAuthentication = true;
+  #   };
+  # };
 
   # https://nixos.wiki/wiki/FAQ/When_do_I_update_stateVersion
   system.stateVersion = "24.11";
