a691e6d3e598f6628c03db809cd2483d4689766c
esp deadbeef <esp0xdeadbeef@gmail.com>
2025-04-14 23:42:51 +0200

    added file and prefix

--- nixos/1-general/packages/nix-specific/packages.nix
+++ nixos/1-general/packages/nix-specific/packages.nix
@@ -1,8 +1,13 @@
 { config, pkgs, ... }:
 {
   environment.systemPackages = with pkgs; [
+
+    # cache indexes with this command:
     nix-index
+    # needed to format nix configs:
     nixfmt-rfc-style
+    # to hack your own packages
+    nix-prefetch
     # this is available a different way ( google it) but not required because we push via nixos rebuilds:
     #home-manager
   ];
