0cf714d3a61075556419681f0cf806adfa305fb3
esp deadbeef <esp0xdeadbeef@gmail.com>
2025-04-15 13:55:47 +0200

    added auto generation for configuration.nix

--- nixos/l-werk/generate-imports.sh
+++ nixos/l-werk/generate-imports.sh
@@ -1,11 +1,15 @@
 #!/usr/bin/env bash
 
 (
+(
     cd /home/deadbeef/github/nixos/nixos/l-werk
     find . | grep -v 'old\|./configuration.nix$' | grep 'nix$'
 ) | sort
-
+echo # empty row
 (
     cd /home/deadbeef/github/nixos/nixos/1-general
     find ../1-general | grep '\.nix$' | grep -v 'is-vm\|autologin\|usb-firewall.nix'
 ) | sort
+) | tee /tmp/includes-l-werk.txt
+
+awk -v r="$(< /tmp/includes-l-werk.txt)" '{gsub(/STRING_TO_REPLACE_WITH_GENERATE_IMPORT.SH/, r)}1' configuration.nix.template > configuration.nix
