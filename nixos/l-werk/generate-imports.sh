cf7f3ae3427f340587ed27427441cd35d043b143
esp deadbeef <esp0xdeadbeef@gmail.com>
2025-04-15 13:14:22 +0200

    added sorts and the new packages

--- nixos/l-werk/generate-imports.sh
+++ nixos/l-werk/generate-imports.sh
@@ -3,9 +3,9 @@
 (
     cd /home/deadbeef/github/nixos/nixos/l-werk
     find . | grep -v 'old\|./configuration.nix$' | grep 'nix$'
-)
+) | sort
 
 (
     cd /home/deadbeef/github/nixos/nixos/1-general
     find ../1-general | grep '\.nix$' | grep -v 'is-vm\|autologin\|usb-firewall.nix'
-)
+) | sort
