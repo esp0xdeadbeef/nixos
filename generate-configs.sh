511c5e75279f57a495bb6043079e4519275652b3
esp0xdeadbeef <esp0xdeadbeef@gmail.com>
2025-04-27 21:57:10 +0000

    using relative paths instead of hardcoded paths

--- generate-configs.sh
+++ generate-configs.sh
@@ -3,46 +3,46 @@
 export CURRENT_CONFIG="l-esp"
 (
     (
-        cd /home/deadbeef/github/nixos/nixos/$CURRENT_CONFIG
+        cd .//nixos/$CURRENT_CONFIG
         find . | grep -v 'old\|./configuration.nix$' | grep 'nix$' | while read line; do
             echo "    $line"
         done
     ) | sort
     echo # empty row
     (
-        cd /home/deadbeef/github/nixos/nixos/1-general
+        cd .//nixos/1-general
         find ../1-general | grep '\.nix$' | grep -v 'llms\|is-vm\|/work/\|/werk/\|autologin\|mxbuild' | while read line; do
             echo "    $line"
         done
     ) | sort
 ) | grep -v '/build_' | tee /tmp/results.txt
 
-awk -v r="$(</tmp/results.txt)" '{gsub(/STRING_TO_REPLACE_WITH_GENERATE_IMPORT.SH/, r)}1' /home/deadbeef/github/nixos/nixos/$CURRENT_CONFIG/build_configuration.nix > /home/deadbeef/github/nixos/nixos/$CURRENT_CONFIG/configuration.nix
+awk -v r="$(</tmp/results.txt)" '{gsub(/STRING_TO_REPLACE_WITH_GENERATE_IMPORT.SH/, r)}1' .//nixos/$CURRENT_CONFIG/build_configuration.nix > .//nixos/$CURRENT_CONFIG/configuration.nix
 
 export CURRENT_CONFIG="l-werk"
 (
     (
-        cd /home/deadbeef/github/nixos/nixos/$CURRENT_CONFIG
+        cd .//nixos/$CURRENT_CONFIG
         find . | grep -v 'old\|./configuration.nix$' | grep 'nix$' | while read line; do
             echo "    $line"
         done
     ) | sort
     echo # empty row
     (
-        cd /home/deadbeef/github/nixos/nixos/1-general
+        cd .//nixos/1-general
         find ../1-general | grep '\.nix$' | grep -v '/build_' | grep -v 'is-vm\|autologin\|usb-firewall.nix' | while read line; do
             echo "    $line"
         done
     ) | sort
 ) | grep -v '/build_'  | tee /tmp/results.txt
 
-awk -v r="$(</tmp/results.txt)" '{gsub(/STRING_TO_REPLACE_WITH_GENERATE_IMPORT.SH/, r)}1' /home/deadbeef/github/nixos/nixos/$CURRENT_CONFIG/build_configuration.nix > /home/deadbeef/github/nixos/nixos/$CURRENT_CONFIG/configuration.nix
+awk -v r="$(</tmp/results.txt)" '{gsub(/STRING_TO_REPLACE_WITH_GENERATE_IMPORT.SH/, r)}1' .//nixos/$CURRENT_CONFIG/build_configuration.nix > .//nixos/$CURRENT_CONFIG/configuration.nix
 
 export CURRENT_CONFIG="l-x13s"
 
 (
     (
-        cd /home/deadbeef/github/nixos/nixos/$CURRENT_CONFIG
+        cd .//nixos/$CURRENT_CONFIG
         find . | grep -v 'old\|./configuration.nix$' | grep 'nix$'| while read line;
         do
             echo "    $line"
@@ -50,7 +50,7 @@ export CURRENT_CONFIG="l-x13s"
     ) | sort
     echo # empty row
     (
-        cd /home/deadbeef/github/nixos/nixos/1-general
+        cd .//nixos/1-general
         find ../1-general | grep '\.nix$' | grep -v '/build_' | grep -v mxbuild | grep -v 'llms\|is-vm\|virtualization\|not-on-aarch64\|/work\|/werk\|1-custom-packages' | while read line;
         do
             echo "    $line"
@@ -58,105 +58,105 @@ export CURRENT_CONFIG="l-x13s"
     ) | sort
 ) | grep -v '/build_'  | tee /tmp/results.txt
 
-awk -v r="$(</tmp/results.txt)" '{gsub(/STRING_TO_REPLACE_WITH_GENERATE_IMPORT.SH/, r)}1' /home/deadbeef/github/nixos/nixos/$CURRENT_CONFIG/build_configuration.nix > /home/deadbeef/github/nixos/nixos/$CURRENT_CONFIG/configuration.nix
+awk -v r="$(</tmp/results.txt)" '{gsub(/STRING_TO_REPLACE_WITH_GENERATE_IMPORT.SH/, r)}1' .//nixos/$CURRENT_CONFIG/build_configuration.nix > .//nixos/$CURRENT_CONFIG/configuration.nix
 
 export CURRENT_CONFIG="s-router-vpn-1"
 (
     (
-        cd /home/deadbeef/github/nixos/nixos/$CURRENT_CONFIG
+        cd .//nixos/$CURRENT_CONFIG
         find . | grep -v 'old\|./configuration.nix$' | grep 'nix$' | while read line; do
             echo "    $line"
         done
     ) | sort
     echo # empty row
     (
-        cd /home/deadbeef/github/nixos/nixos/1-general
+        cd .//nixos/1-general
         find ../1-general | grep '\.nix$' | grep -v '/build_' | grep -v 'usb-firewall.nix\|/virtualization/\|llms\|/network/\|browsers\|graphics\|pdf\|rdp\|scripting-languages\|darkmode\|pentesting\|usb-tools\|1-custom-packages\|services/\|audio\|nix-specific' | while read line; do
             echo "    $line"
         done
     ) | sort
 ) | grep -v '/build_' | tee /tmp/results.txt
 
-awk -v r="$(</tmp/results.txt)" '{gsub(/STRING_TO_REPLACE_WITH_GENERATE_IMPORT.SH/, r)}1' /home/deadbeef/github/nixos/nixos/$CURRENT_CONFIG/build_configuration.nix >  /home/deadbeef/github/nixos/nixos/$CURRENT_CONFIG/configuration.nix
+awk -v r="$(</tmp/results.txt)" '{gsub(/STRING_TO_REPLACE_WITH_GENERATE_IMPORT.SH/, r)}1' .//nixos/$CURRENT_CONFIG/build_configuration.nix >  .//nixos/$CURRENT_CONFIG/configuration.nix
 
 
 
 export CURRENT_CONFIG="s-test-vm"
 (
     (
-        cd /home/deadbeef/github/nixos/nixos/$CURRENT_CONFIG
+        cd .//nixos/$CURRENT_CONFIG
         find . | grep -v 'old\|./configuration.nix$' | grep 'nix$' | while read line; do
             echo "    $line"
         done
     ) | sort
     echo # empty row
     (
-        cd /home/deadbeef/github/nixos/nixos/1-general
+        cd .//nixos/1-general
         find ../1-general | grep '\.nix$' | grep -v '/build_' | grep -v 'usb-firewall.nix\|/virtualization/\|llms\|browsers\|graphics\|pdf\|rdp\|scripting-languages\|pentesting\|usb-tools\|1-custom-packages\|services/\|audio' | while read line; do
             echo "    $line"
         done
     ) | sort
 ) | grep -v '/build_' | tee /tmp/results.txt
 
-awk -v r="$(</tmp/results.txt)" '{gsub(/STRING_TO_REPLACE_WITH_GENERATE_IMPORT.SH/, r)}1' /home/deadbeef/github/nixos/nixos/$CURRENT_CONFIG/build_configuration.nix >  /home/deadbeef/github/nixos/nixos/$CURRENT_CONFIG/configuration.nix
+awk -v r="$(</tmp/results.txt)" '{gsub(/STRING_TO_REPLACE_WITH_GENERATE_IMPORT.SH/, r)}1' .//nixos/$CURRENT_CONFIG/build_configuration.nix >  .//nixos/$CURRENT_CONFIG/configuration.nix
 
 
 export CURRENT_CONFIG="s-test-vm-impermanence"
 (
     (
-        cd /home/deadbeef/github/nixos/nixos/$CURRENT_CONFIG
+        cd .//nixos/$CURRENT_CONFIG
         find . | grep -v 'old\|./configuration.nix$' | grep 'nix$' | while read line; do
             echo "    $line"
         done
     ) | sort
     echo # empty row
     (
-        cd /home/deadbeef/github/nixos/nixos/1-general
+        cd .//nixos/1-general
         find ../1-general | grep '\.nix$' | grep -v '/build_' | grep -v 'usb-firewall.nix\|/virtualization/\|llms\|browsers\|graphics\|pdf\|rdp\|scripting-languages\|pentesting\|usb-tools\|1-custom-packages\|services/\|audio' | while read line; do
             echo "    $line"
         done
     ) | sort
 ) | grep -v '/build_' | tee /tmp/results.txt
 
-awk -v r="$(</tmp/results.txt)" '{gsub(/STRING_TO_REPLACE_WITH_GENERATE_IMPORT.SH/, r)}1' /home/deadbeef/github/nixos/nixos/$CURRENT_CONFIG/build_configuration.nix >  /home/deadbeef/github/nixos/nixos/$CURRENT_CONFIG/configuration.nix
+awk -v r="$(</tmp/results.txt)" '{gsub(/STRING_TO_REPLACE_WITH_GENERATE_IMPORT.SH/, r)}1' .//nixos/$CURRENT_CONFIG/build_configuration.nix >  .//nixos/$CURRENT_CONFIG/configuration.nix
 
 
 export CURRENT_CONFIG="s-test-vm-impermanence-2"
 (
     (
-        cd /home/deadbeef/github/nixos/nixos/$CURRENT_CONFIG
+        cd .//nixos/$CURRENT_CONFIG
         find . | grep -v 'old\|./configuration.nix$' | grep 'nix$' | while read line; do
             echo "    $line"
         done
     ) | sort
     echo # empty row
     (
-        cd /home/deadbeef/github/nixos/nixos/1-general
+        cd .//nixos/1-general
         find ../1-general | grep 'nmcli\|environment\|terminal-optimisers'| grep '\.nix$' | grep -v '/build_' | grep -v 'usb-firewall.nix\|/virtualization/\|llms\|browsers\|graphics\|pdf\|rdp\|scripting-languages\|pentesting\|usb-tools\|1-custom-packages\|services/\|audio' | while read line; do
             echo "    $line"
         done
     ) | sort
 ) | grep -v '/build_' | tee /tmp/results.txt
 
-awk -v r="$(</tmp/results.txt)" '{gsub(/STRING_TO_REPLACE_WITH_GENERATE_IMPORT.SH/, r)}1' /home/deadbeef/github/nixos/nixos/$CURRENT_CONFIG/build_configuration.nix >  /home/deadbeef/github/nixos/nixos/$CURRENT_CONFIG/configuration.nix
+awk -v r="$(</tmp/results.txt)" '{gsub(/STRING_TO_REPLACE_WITH_GENERATE_IMPORT.SH/, r)}1' .//nixos/$CURRENT_CONFIG/build_configuration.nix >  .//nixos/$CURRENT_CONFIG/configuration.nix
 
 
 export CURRENT_CONFIG="s-lxc-test"
 (
     (
-        cd /home/deadbeef/github/nixos/nixos/$CURRENT_CONFIG
+        cd .//nixos/$CURRENT_CONFIG
         find . | grep -v 'old\|./configuration.nix$' | grep 'nix$' | while read line; do
             echo "    $line"
         done
     ) | sort
     echo # empty row
     (
-        cd /home/deadbeef/github/nixos/nixos/1-general
+        cd .//nixos/1-general
         find ../1-general | grep '\.nix$' | grep -v '/build_'  | while read line; do
             echo "    $line"
         done
     ) | sort
 ) | grep -v '/build_' | tee /tmp/results.txt
 
-awk -v r="$(</tmp/results.txt)" '{gsub(/STRING_TO_REPLACE_WITH_GENERATE_IMPORT.SH/, r)}1' /home/deadbeef/github/nixos/nixos/$CURRENT_CONFIG/build_configuration.nix >  /home/deadbeef/github/nixos/nixos/$CURRENT_CONFIG/configuration.nix
+awk -v r="$(</tmp/results.txt)" '{gsub(/STRING_TO_REPLACE_WITH_GENERATE_IMPORT.SH/, r)}1' .//nixos/$CURRENT_CONFIG/build_configuration.nix >  .//nixos/$CURRENT_CONFIG/configuration.nix
 
