6e148b9ddb5ad71af774cc23896294a2a44973b3
esp deadbeef <esp0xdeadbeef@gmail.com>
2025-05-02 00:28:09 +0200

    Added the GPU load to the nixos config

--- home-manager/l-esp/configs/i3status-rust/packages.nix
+++ home-manager/l-esp/configs/i3status-rust/packages.nix
@@ -8,6 +8,40 @@
 
 {
 
+home.file."/.config/i3status-rust/gpu-load.sh" = {
+    text = ''
+#!/usr/bin/env bash
+
+# Get GPU load percentage
+#GPU_LOAD_nvidia=$(nvidia-smi --query-gpu=utilization.gpu --format=csv,noheader,nounits)
+GPU_LOAD_amd=$(${pkgs.rocmPackages.rocm-smi}/bin/rocm-smi --showuse | grep "GPU use" | rev | awk '{print $1}')
+
+GPU_LOAD=$(echo $GPU_LOAD_amd)
+# Determine the state based on GPU load percentage
+if [ "$GPU_LOAD" -le 25 ]; then
+  STATE="Idle"
+  TEXT=""
+elif [ "$GPU_LOAD" -le 50 ]; then
+  STATE="Info"
+  TEXT=""
+elif [ "$GPU_LOAD" -le 75 ]; then
+  STATE="Good"
+  TEXT=""
+elif [ "$GPU_LOAD" -le 90 ]; then
+  STATE="Warning"
+  TEXT=""
+else
+  STATE="Critical"
+  TEXT=""
+fi
+TEXT="GPU: $GPU_LOAD%"
+# Output the JSON block
+echo "{\"icon\":\"\",\"state\":\"$STATE\", \"text\": \"$TEXT\"}"
+    '';
+    executable = true;
+  };
+
+
 sops.templates.i3status-rust = {
   content = ''
 # Config for i3blocks-rs
@@ -44,8 +78,8 @@ block = "custom"
 cycle = [
     "(echo -n '🏠 '; ip a s wlp5s0 | grep inet | grep dynamic | grep -v temp | awk '{print $2}' | sed 's|/.*||g' | tail -n2 | head -n1)",
     "(echo -n '🏠 '; ip a s wlp5s0 | grep inet | grep dynamic | grep -v temp | awk '{print $2}' | sed 's|/.*||g' | tail -n1 | head -n1)",
-    "(echo -n '🌍 '; curl -s ifconfig.me -4)",
-    "(echo -n '🌍 '; curl -s ifconfig.me -6)",
+    "(echo -n '🌍 '; ${pkgs.curl}/bin/curl -s ifconfig.me -4)",
+    "(echo -n '🌍 '; ${pkgs.curl}/bin/curl -s ifconfig.me -6)",
 ]
 
 interval = 60
