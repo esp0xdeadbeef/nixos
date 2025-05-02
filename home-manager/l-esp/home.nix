4879c44078a7f42efe0e418223f0a0a9bd7368f5
new name <esp0xdeadbeef@gmail.com>
2025-05-03 01:15:55 +0200

    afasdf

--- home-manager/l-esp/home.nix
+++ home-manager/l-esp/home.nix
@@ -101,7 +101,7 @@ in
 
   # Enable home-manager and git
   programs.home-manager.enable = true;
-  programs.git.enable = true;
+  # programs.git.enable = true;
 
   # Nicely reload system units when changing configs
   systemd.user.startServices = "sd-switch";
