3800016b8a4527a605166e9ac291f936c9ff839d
esp0xdeadbeef <esp0xdeadbeef@gmail.com>
2025-05-04 15:43:47 +0200

    nas working :)

--- nixos/l-esp/connect-nas/default.nix
+++ nixos/l-esp/connect-nas/default.nix
@@ -1,63 +1,53 @@
 { config, pkgs, lib, ... }:
 
 let
-  mountBin = "${pkgs.cifs-utils}/bin/mount.cifs";   # full path!
-
-  mkScript = shareKey: name:
-    pkgs.writeShellScript "mount-nas-${name}" ''
-      set -e
-      IP=$(cat ${config.sops.secrets."nas-ip".path})
-      SHARE=$(cat ${config.sops.secrets."${shareKey}".path})
-      USER=$(cat ${config.sops.secrets."nas-username".path})
-      PASS=$(cat ${config.sops.secrets."nas-password".path})
-
-      exec ${mountBin} //"$IP"/"$SHARE" /mnt/qnap/${name} \
-          -o username="$USER",password="$PASS",uid=1000,gid=100,iocharset=utf8,vers=3.0
-    '';
-
-  privateScript = mkScript "nas-share-private" "private";
-  publicScript  = mkScript "nas-share-public"  "public";
-in
-{
-  # declare all secrets
-  sops.secrets."nas-ip"            = {};
-  sops.secrets."nas-username"      = {};
-  sops.secrets."nas-password"      = {};
-  sops.secrets."nas-share-private" = {};
-  sops.secrets."nas-share-public"  = {};
-
-  # dirs
-  systemd.tmpfiles.rules = [
-    "d /mnt/qnap/private 0755 root root -"
-    "d /mnt/qnap/public  0755 root root -"
+  # Common CIFS mount options with systemd automount
+  automount_opts = lib.concatStringsSep "," [
+    "x-systemd.automount"
+    "noauto"
+    "x-systemd.idle-timeout=600"
+    "x-systemd.device-timeout=5s"
+    "x-systemd.mount-timeout=5s"
   ];
-
+in {
+  # Ensure the CIFS mount helper is available
   environment.systemPackages = [ pkgs.cifs-utils ];
 
-  # oneshot mount services
-  systemd.services.mount-nas-private = {
-    description = "Mount NAS Private share (on‑demand)";
-    serviceConfig = {
-      Type = "oneshot";
-      ExecStart = privateScript;
-      RemainAfterExit = true;
-    };
+  # Declare SOPS secrets so they can be referenced
+  sops.secrets = {
+    "nas-ip"            = {};
+    "nas-username"      = {};
+    "nas-password"      = {};
+    "nas-share-public"  = {};
+    "nas-share-private" = {};
   };
 
-  systemd.services.mount-nas-public = {
-    description = "Mount NAS Public share (on‑demand)";
-    serviceConfig = {
-      Type = "oneshot";
-      ExecStart = publicScript;
-      RemainAfterExit = true;
-    };
+  # Generate credential files from SOPS secrets
+  environment.etc = {
+    "smb-secrets-private".text = lib.concatStringsSep "\n" [
+      "username=${builtins.readFile config.sops.secrets.nas-username.path}"
+      "password=${builtins.readFile config.sops.secrets.nas-password.path}"
+    ];
+    "smb-secrets-public".text = lib.concatStringsSep "\n" [
+      "username=${builtins.readFile config.sops.secrets.nas-username.path}"
+      "password=${builtins.readFile config.sops.secrets.nas-password.path}"
+    ];
   };
 
-  # automounts that trigger them
-  systemd.automounts = [
-    { where = "/mnt/qnap/private"; wantedBy = [ "multi-user.target" ];
-      automountConfig.TimeoutIdleSec = "600"; requires = [ "mount-nas-private.service" ]; }
-    { where = "/mnt/qnap/public";  wantedBy = [ "multi-user.target" ];
-      automountConfig.TimeoutIdleSec = "600"; requires = [ "mount-nas-public.service"  ]; }
-  ];
+  # Declarative CIFS mounts with systemd automount
+  fileSystems = {
+    # Private share
+    "/mnt/nas/private" = {
+      device = "//${builtins.readFile config.sops.secrets.nas-ip.path}/${builtins.readFile config.sops.secrets.nas-share-private.path}";
+      fsType = "cifs";
+      options = [ "${automount_opts},credentials=/etc/smb-secrets-private,uid=1000,gid=100,iocharset=utf8,vers=3.0" ];
+    };
+
+    # Public share
+    "/mnt/nas/public" = {
+      device = "//${builtins.readFile config.sops.secrets.nas-ip.path}/${builtins.readFile config.sops.secrets.nas-share-public.path}";
+      fsType = "cifs";
+      options = [ "${automount_opts},credentials=/etc/smb-secrets-public,uid=1000,gid=100,iocharset=utf8,vers=3.0" ];
+    };
+  };
 }
