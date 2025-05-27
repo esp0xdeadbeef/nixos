77032a8eb7c1830916b1a34cc75cbb599347bd59
esp0xdeadbeef <esp0xdeadbeef@gmail.com>
2025-05-27 23:43:47 +0000

    test if it still works on older 24.11

--- nixos/l-werk/hardware/audio-and-bluetooth.nix
+++ nixos/l-werk/hardware/audio-and-bluetooth.nix
@@ -3,7 +3,7 @@
   #############################
   # Audio And Bluetooth
   #############################
-  hardware.pulseaudio.enable = false;
+  services.pulseaudio.enable = false;
   security.rtkit.enable = true;
   services.pipewire = {
     enable = true;
