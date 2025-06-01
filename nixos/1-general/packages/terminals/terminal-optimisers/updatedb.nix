66d446e8be716e22755fef593c6297c9871ffe8c
esp0xdeadbeef <esp0xdeadbeef@gmail.com>
2025-06-01 21:53:23 +0200

    still perms issues, but will be resolved someday

--- nixos/1-general/packages/terminals/terminal-optimisers/updatedb.nix
+++ nixos/1-general/packages/terminals/terminal-optimisers/updatedb.nix
@@ -1,47 +1,49 @@
-{
-  config,
-  lib,
-  pkgs,
-  ...
-}:
+{ config, lib, pkgs, ... }:
 
 let
-  users = lib.attrNames (lib.filterAttrs (name: user: user.isNormalUser) config.users.users);
+  # 1) Grab the original users.users set so we can compute the list of normal-user names
+  allUsers = config.users.users;
 
-  # Helper: build an absolute path for every user
-  mkUserPaths =
-    u:
-    map (p: "/home/${u}/${p}") [
-      ".vscode"
-      ".config/Code"
-      ".local/share/lxc"
-      ".local/share/containers"
-    ];
+  # 2) Filter out only the “isNormalUser = true” entries, then grab their names
+  userNames = lib.attrNames (lib.filterAttrs (_: user: user.isNormalUser) allUsers);
 
-  userPrunes = lib.flatten (map mkUserPaths users);
-
+  # 3) Build your userPrunes exactly as before
+  mkUserPaths = u: map (p: "/home/${u}/${p}") [
+    ".vscode"
+    ".config/Code"
+    ".local/share/lxc"
+    ".local/share/containers"
+  ];
+  userPrunes = lib.flatten (map mkUserPaths userNames);
 in
 {
-  services.locate = {
-    enable = true;
-    package = pkgs.plocate;
-    # localuser = null;
-    output = "/persist/var/cache/locatedb"; # where to write the DB
+  ######################################################################
+  # A) Instead of touching `users.users`, we let the locate module make
+  #    a UNIX group called “mlocate” and then we populate that group’s
+  #    member list with every normal user.
+  ######################################################################
+  users.groups.mlocate = {
+    # If you don’t care about specifying a fixed GID, you can omit “gid = …;”
+    members = userNames;
+  };
 
+  ######################################################################
+  # B) Enable mlocate so that it writes its DB into /persist/var/cache/locatedb
+  ######################################################################
+  services.locate = {
+    enable     = true;
+    package    = pkgs.mlocate;
+    output     = "/persist/var/cache/locatedb";
     prunePaths = lib.mkAfter (
-      [
-        "/partition-root"
-        "/persist"
-        "/mnt/nas"
-        "/nix"
-        "/var/lib/flatpak"
-      ]
+      [ "/partition-root" "/persist" "/mnt/nas" "/nix" "/var/lib/flatpak" ]
       ++ userPrunes
     );
-
-    interval = "hourly"; # Update the locate database every 2 hours
-    #   OnBootSec = "5min"; # Run 5 minutes after startup
-    #   OnUnitActiveSec = "2h"; # Run every 2 hours
-    # };
+    interval   = "hourly";
   };
+
+  ######################################################################
+  # C) Make sure `locate` actually reads the DB at /persist/var/cache/locatedb
+  #    instead of defaulting to /var/cache/locatedb.
+  ######################################################################
+  environment.variables.LOCATE_PATH = "/persist/var/cache/locatedb";
 }
