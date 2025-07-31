183de5a1913fa56523c2352a0741d870a189d665
esp0xdeadbeef <esp0xdeadbeef@l-x13s.lan>
2025-07-31 23:18:33 +0200

    moved amd to bak, added nvidia

---
+++ nixos/l-esp/hardware/nvidia.nix
@@ -0,0 +1,43 @@
+{
+  pkgs,
+  config,
+  libs,
+  ...
+}:
+{
+  nixpkgs.config.allowUnfree = true;
+  hardware.nvidia-container-toolkit.enable = true;
+
+  environment.systemPackages = with pkgs; [
+    cudaPackages.cudatoolkit
+  ];
+
+  hardware.graphics = {
+    enable = true;
+    enable32Bit = true;
+  };
+
+  services.xserver.videoDrivers = [ "nvidia" ];
+
+  hardware.nvidia = {
+    modesetting.enable = true;
+    powerManagement.enable = false;
+    open = false;
+    nvidiaSettings = true;
+    package = config.boot.kernelPackages.nvidiaPackages.stable;
+    prime = {
+      offload = {
+        enable = true;
+        enableOffloadCmd = true;
+      };
+      intelBusId = "PCI:0:2:0";
+      nvidiaBusId = "PCI:1:0:0";
+    };
+  };
+
+  virtualisation.docker = {
+    enable = true;
+    enableOnBoot = true;
+    package = pkgs.docker;
+  };
+}
