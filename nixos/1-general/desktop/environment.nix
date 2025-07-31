23542e05449baa94e5894abb6181b98615c2b6ad
esp0xdeadbeef <esp0xdeadbeef@l-x13s.lan>
2025-08-01 00:51:04 +0200

    added gnome also

--- nixos/1-general/desktop/environment.nix
+++ nixos/1-general/desktop/environment.nix
@@ -56,7 +56,7 @@
   services.xserver.displayManager.gdm.enable = true;
   # services.xserver.displayManager.sddm.enable = true;
   
-  # services.xserver.desktopManager.gnome.enable = true;
+  services.xserver.desktopManager.gnome.enable = true;
 
   #programs.sway.enable = true;
   services.displayManager.defaultSession = "none+i3";
