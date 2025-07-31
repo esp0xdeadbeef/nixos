84dd052313e370293ff1dfbae7c5383e87555a36
esp0xdeadbeef <esp0xdeadbeef@l-x13s.lan>
2025-07-31 23:04:20 +0200

    added home-manager to the build and ran build

--- nixos/l-esp/configuration.nix
+++ nixos/l-esp/configuration.nix
@@ -32,6 +32,7 @@
     ./hardware/lanzaboote.nix
     ./hardware/steam.nix
     ./hardware/swap-and-tmpfs.nix
+    ./hardware/temp.nix
     ./llms/llms.nix
     ./osep/bind-to-lxc.nix
     ./osep/scraping-osep-material.nix
