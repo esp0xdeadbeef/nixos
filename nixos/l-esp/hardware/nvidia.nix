6daca7e81cf511d4595a0e57a7803c133ec9bcb1
esp0xdeadbeef <esp0xdeadbeef@l-x13s.lan>
2025-08-02 01:59:32 +0200

    hacked sway, otherwise it won't start.

--- nixos/l-esp/hardware/nvidia.nix
+++ nixos/l-esp/hardware/nvidia.nix
@@ -1,36 +1,43 @@
 {
-  pkgs,
   config,
+  pkgs,
   lib,
   ...
 }:
-{
-  # services.xserver.videoDrivers = [ "nvidia" ];
-  # hardware.nvidia = {
-  #   open = true;
-  #   modesetting.enable = true;
-  #   nvidiaSettings = true;
-  #   prime = {
-  #     sync.enable = false; # gpu always
-  #     offload.enable = false; # gpu on demand
-  #     #nvidiaBusId = "PCI:10:0:0"; #epgu
-  #     nvidiaBusId = "PCI:1:0:0"; # dedicated gpu
-  #     intelBusId = "PCI:0:2:0";
-  #   };
-  # };
-  # environment.sessionVariables = {
-  #   __GLX_VENDOR_LIBRARY_NAME = "nvidia";
-  #   GBM_BACKEND = "nvidia-drm";
-  #   WLR_NO_HARDWARE_CURSORS = "1";
-  #   WLR_RENDERER_ALLOW_SOFTWARE = "1";
-  # };
 
-  # # # required for external monitor usage on nvidia offload
-  # # specialisation = {
-  # #   external-display.configuration = {
-  # #     system.nixos.tags = [ "external-display" ];
-  # #     hardware.nvidia.prime.offload.enable = lib.mkForce false;
-  # #     hardware.nvidia.powerManagement.enable = lib.mkForce false;
-  # #   };
-  # # };
+{
+  boot.initrd.kernelModules = [
+    "i915"
+    "nvidia"
+  ];
+  services.xserver.enable = false;
+  services.xserver.displayManager.gdm.enable = false;
+  services.displayManager.sddm.enable = false;
+  services.xserver.videoDrivers = [
+    "modesetting"
+    "nvidia"
+  ];
+  hardware.nvidia = {
+    open = true;
+    package = config.boot.kernelPackages.nvidiaPackages.stable;
+    modesetting.enable = true;
+    prime = {
+      sync.enable = false;
+      offload.enable = false;
+      nvidiaBusId = "PCI:1:0:0";
+      intelBusId = "PCI:0:2:0";
+    };
+  };
+
+  environment.variables = {
+    WLR_DRM_DEVICES = "/dev/dri/card1:/dev/dri/card0";
+    WLR_NO_HARDWARE_CURSORS = "1";
+    WLR_RENDERER_ALLOW_SOFTWARE = "1";
+  };
+
+  environment.shellAliases = {
+    sway = "sway --unsupported-gpu";
+  };
+
+  programs.sway.enable = true;
 }
