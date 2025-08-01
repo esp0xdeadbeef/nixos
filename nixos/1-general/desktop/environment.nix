ca38df563410862cb13d7e1032bcab56455faf46
esp0xdeadbeef <esp0xdeadbeef@l-x13s.lan>
2025-08-01 04:01:37 +0200

    back to original + xwayland for i3 wm

--- nixos/1-general/desktop/environment.nix
+++ nixos/1-general/desktop/environment.nix
@@ -49,15 +49,18 @@
       variant = "";
     };
   };
+  services.displayManager.defaultSession = "none+i3";
+  # services.xserver.displayManager.gdm.enable = true;
+  programs.xwayland.enable = true;
 
   #environment.variables.GTK_THEME = "Adwaita:dark";
   services.gnome.gnome-keyring.enable = true;
-  # services.desktopManager.plasma6.enable = true;
-  # services.xserver.displayManager.gdm.enable = true;
+  services.desktopManager.plasma6.enable = true;
+  services.xserver.displayManager.gdm.enable = true;
   # services.displayManager.sddm.enable = true;
-  
+
   # services.xserver.desktopManager.gnome.enable = true;
 
-  # programs.sway.enable = true;
+  programs.sway.enable = true;
   # services.displayManager.defaultSession = "none+i3";
 }
