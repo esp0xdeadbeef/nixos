14624118883d69ff6dc4afb07ad9d80b2f7dab2f
esp0xdeadbeef <esp0xdeadbeef@gmail.com>
2025-09-02 22:55:59 +0200

    made sure gc will run

--- nixos/01-general/system/garbage-collection.nix
+++ nixos/01-general/system/garbage-collection.nix
@@ -2,8 +2,11 @@
 {
   nix.gc = {
     automatic = true;
-    persistent = false;
-    dates = "daily";
+    persistent = true;
+    # dates = "daily";
     options = "--delete-older-than 30d";
+    dates = "17:00";                    # base time
+    randomizedDelaySec = "15min";      # run somewhere between 18:00 and 18:15
+    
   };
 }
