54d5f399394c77ecf5cf6863337341c95b0c629e
esp0xdeadbeef <esp0xdeadbeef@gmail.com>
2025-09-02 15:50:18 +0200

    added new rsync scripts

---
+++ rsync-update-reboot-s-lxc-test.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+
+if rsync -va /home/deadbeef/github/nixos deadbeef@s-lxc-test:~/github/ | grep -q 'nixos/'; then
+  echo switching and rebooting...
+  ssh deadbeef@s-lxc-test 'sudo nixos-rebuild switch --impure --flake path:/home/deadbeef/github/nixos#$(hostname) --no-write-lock-file' && ssh deadbeef@s-lxc-test 'sudo reboot' && echo "rebooting!" && sleep 30
+else
+  echo sleeping:
+  sleep 2
+fi
