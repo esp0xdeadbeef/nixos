9196d415b06c139766da6f31bf99d1a0e0cb21ac
esp0xdeadbeef-l-werk <esp0xdeadbeef@gmail.com>
2025-09-09 11:44:53 +0200

    not working seclists (feroxbuster)

--- nixos/l-werk-1/work-packages/wordlists/seclists/packages.nix
+++ nixos/l-werk-1/work-packages/wordlists/seclists/packages.nix
@@ -1,10 +1,18 @@
-{ config, pkgs, lib, ... }:
+{
+  config,
+  pkgs,
+  lib,
+  ...
+}:
 
 let
   wl = pkgs.wordlists.override { lists = [ pkgs.seclists ]; };
-in {
+in
+{
   environment.systemPackages = [ wl ];
-  environment.etc."seclists".source =
-    "${wl}/share/wordlists/seclists"; # /etc/seclists -> Nix store
-  environment.pathsToLink = [ "/share/wordlists/seclists" ]; # optional
+
+  systemd.tmpfiles.rules = [
+    "L /usr/share/seclists - - - - ${wl}/share/wordlists/seclists"
+  ];
+
 }
