0b4bda4e80ea9a7e760726bd4a1315d5f611b12e
esp0xdeadbeef <esp0xdeadbeef@gmail.com>
2025-09-13 05:52:27 +0200

    added specific builder

--- dev-updaters/rsync-update-reboot.sh
+++ dev-updaters/rsync-update-reboot.sh
@@ -12,12 +12,15 @@ echo "[*] Checking uptime on $HOST..."
 ssh "deadbeef@$HOST" uptime || { echo "❌ SSH to $HOST failed"; sleep 2; exit 1; }
 
 echo "[*] Running rsync to $HOST..."
-if rsync -va --exclude='.git' /home/deadbeef/github/nixos "deadbeef@$HOST:~/github/" | grep -q 'nixos/'; then
+if timeout 3 rsync -va --exclude='.git' /home/deadbeef/github/nixos "deadbeef@$HOST:~/github/" | grep -q "nixos/$HOST"; then
   echo "[*] Changes detected, rebuilding and rebooting $HOST..."
+  if [[ "$HOST" == "s-router-vpn-1" ]]; then
+    ( ssh "deadbeef@$HOST" 'sudo nmcli connection down tun0' ) || true
+  fi
   ssh "deadbeef@$HOST" 'sudo nixos-rebuild boot --impure --flake path:/home/deadbeef/github/nixos#$(hostname) --no-write-lock-file' \
     && ssh "deadbeef@$HOST" 'sudo reboot' \
     && echo "[*] Rebooting $HOST..." \
-    && sleep 30
+    && sleep 20
 else
   echo "[*] No changes, sleeping"
   sleep 2
