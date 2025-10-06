1f64a320b2b0b9ca8cc3d855a64b13952f3fe025
esp0xdeadbeef <esp0xdeadbeef@gmail.com>
2025-10-07 00:27:19 +0200

    no need to resolve the hostname, this is fine.

--- nixos/01-general/system/autoupdate.nix
+++ nixos/01-general/system/autoupdate.nix
@@ -1,14 +1,10 @@
 { config, pkgs, lib, ... }:
-let
-  hostname = builtins.readFile "/etc/hostname";  # Read the hostname dynamically
-in {
-  #############################
-  # Auto update with dynamic hostname
-  #############################
+
+{
   system.autoUpgrade = {
     enable = true;
-    flake = "github:esp0xdeadbeef/nixos#" + lib.strings.removeSuffix "\n" hostname;
-    flags = [ "--impure" "--no-write-lock-file" ]; # Remove --no-write-lock-file and --upgrade
+    flake = "github:esp0xdeadbeef/nixos#" + config.networking.hostName;
+    flags = [ "--impure" "--no-write-lock-file" ];
     dates = "17:30";
     randomizedDelaySec = "45min";
     operation = "boot";
