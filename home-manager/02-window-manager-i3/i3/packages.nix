209b0de8455ec17f053547958b73f30bc0c7726d
esp0xdeadbeef <esp0xdeadbeef@gmail.com>
2025-10-18 17:25:02 +0200

    removed maestral_qt from i3
    It's already executed in the service, which is cleaner.

--- home-manager/02-window-manager-i3/i3/packages.nix
+++ home-manager/02-window-manager-i3/i3/packages.nix
@@ -355,7 +355,7 @@
       exec_always --no-startup-id ${pkgs.autotiling}/bin/autotiling
 
       # dropbox is whining about shit:
-      exec --no-startup-id ${pkgs.maestral-gui}/bin/maestral_qt
+      #exec --no-startup-id ${pkgs.maestral-gui}/bin/maestral_qt
       
       # exec_always --no-startup-id ${pkgs.dropbox}/bin/dropbox
       exec_always --no-startup-id ${pkgs.spotify}/bin/spotify
