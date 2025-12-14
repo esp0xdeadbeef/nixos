8a348a0fa019b6530cde07860bf53f82e0e1b645
esp0xdeadbeef <esp0xdeadbeef@gmail.com>
2025-12-14 06:01:49 +0100

    just ipv4, still working on ipv6

--- nixos/s-router-impermanence/container.nix
+++ nixos/s-router-impermanence/container.nix
@@ -1,20 +1,22 @@
 { config, pkgs, lib, inputs, ... }:
 
 {
-
-  # SOPS PPP secrets
+  #################################
+  # SOPS PPP SECRETS
+  #################################
   sops.secrets.pppoe-username = { };
   sops.secrets.pppoe-password = { };
-  ############################
+
+  #################################
   # HOST NETWORKING (PURE L2)
-  ############################
+  #################################
   networking.useNetworkd = true;
   networking.networkmanager.enable = false;
   systemd.network.enable = true;
 
-  ############################
+  #################################
   # KERNEL / PPP
-  ############################
+  #################################
   boot.kernelModules = [
     "ppp_generic"
     "pppox"
@@ -26,9 +28,9 @@
     KERNEL=="ppp", MODE="0666"
   '';
 
-  ############################
+  #################################
   # VLAN 6 -> BRIDGE
-  ############################
+  #################################
   systemd.network.netdevs."10-ens19-vlan6" = {
     netdevConfig = {
       Name = "ens19.6";
@@ -75,9 +77,9 @@
     };
   };
 
-  ############################
-  # DISABLE RA
-  ############################
+  #################################
+  # DISABLE RA (HOST)
+  #################################
   boot.kernel.sysctl = {
     "net.ipv6.conf.all.accept_ra" = 0;
     "net.ipv6.conf.default.accept_ra" = 0;
@@ -106,25 +108,59 @@
       "CAP_NET_RAW"
     ];
 
-    ############################
-    # CONTAINER SYSTEM
-    ############################
-    config = { pkgs, ... }: {
+    config = { pkgs, lib, ... }: {
+
+      system.stateVersion = "24.11";
+
+      #################################
+      # BASE
+      #################################
+      services.dbus.enable = true;
+
+      environment.systemPackages = with pkgs; [
+        networkmanager
+        ppp
+        kea
+        iproute2
+        tcpdump
+        curl
+        bind.dnsutils
+      ];
+
+      #################################
+      # DNS
+      #################################
       environment.etc."resolv.conf".enable = false;
+      services.resolved.enable = false;
 
+      systemd.tmpfiles.rules = [
+        "L+ /etc/resolv.conf - - - - /run/NetworkManager/resolv.conf"
+      ];
 
+      networking.useDHCP = lib.mkForce false;
+      networking.useHostResolvConf = lib.mkForce false;
+
+      #################################
+      # DISABLE RA (CONTAINER)
+      #################################
+      boot.kernel.sysctl = {
+        "net.ipv6.conf.all.accept_ra" = 0;
+        "net.ipv6.conf.default.accept_ra" = 0;
+      };
+
+      #################################
+      # NETWORKMANAGER (IPv4 ONLY)
+      #################################
       networking.useNetworkd = false;
+
       networking.networkmanager = {
         enable = true;
         dns = "default";
       };
 
-      services.dbus.enable = true;
-
-      ############################
-      # PERSISTENT PPPoE PROFILE
-      # (SUPPORTED METHOD)
-      ############################
+      #################################
+      # PPPoE PROFILE (IPv6 OFF)
+      #################################
       environment.etc."NetworkManager/system-connections/isp-pppoe.nmconnection" = {
         mode = "0600";
         text = ''
@@ -144,32 +180,63 @@ password=${builtins.readFile config.sops.secrets.pppoe-password.path}
 method=auto
 
 [ipv6]
-addr-gen-mode=default
-method=auto
+method=disabled
 
 [proxy]
         '';
       };
-      
-      services.resolved.enable = false;
-      systemd.tmpfiles.rules = [
-        "L+ /etc/resolv.conf - - - - /run/NetworkManager/resolv.conf"
-      ];
-      systemd.services.resolvconf.enable = false;
 
-      networking.useDHCP = lib.mkForce false;
-      networking.useHostResolvConf = lib.mkForce false;
+      #################################
+      # KEA DHCPv6 CLIENT CONFIG
+      #################################
+      environment.etc."kea/kea-dhcp6.conf".text = ''
+{
+  "Dhcp6": {
+    "interfaces-config": {
+      "interfaces": [ "ppp0" ]
+    },
 
-      environment.systemPackages = with pkgs; [
-        networkmanager
-        ppp
-        iproute2
-        tcpdump
-        curl
-        bind.dnsutils
-      ];
+    "lease-database": {
+      "type": "memfile",
+      "persist": true,
+      "name": "/var/lib/kea/dhcp6.leases"
+    },
 
-      system.stateVersion = "24.11";
+    "preferred-lifetime": 7200,
+    "valid-lifetime": 14400,
+
+    "dhcp-ddns": {
+      "enable-updates": false
+    },
+
+    "pd-pools": [
+      {
+        "prefix": "::",
+        "prefix-len": 48,
+        "delegated-len": 48
+      }
+    ]
+  }
+}
+      '';
+
+      #################################
+      # KEA DHCPv6 CLIENT SERVICE
+      #################################
+      systemd.services.kea-dhcp6 = {
+        description = "Kea DHCPv6 client (IA_NA + IA_PD /48) on ppp0";
+        wantedBy = [ "multi-user.target" ];
+        after = [ "network-online.target" ];
+        wants = [ "network-online.target" ];
+
+        serviceConfig = {
+          ExecStart = "${pkgs.kea}/bin/kea-dhcp6 -c /etc/kea/kea-dhcp6.conf";
+          Restart = "always";
+          RestartSec = 5;
+          CapabilityBoundingSet = [ "CAP_NET_ADMIN" "CAP_NET_RAW" ];
+          AmbientCapabilities = [ "CAP_NET_ADMIN" "CAP_NET_RAW" ];
+        };
+      };
     };
   };
 }
