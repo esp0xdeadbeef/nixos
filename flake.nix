38c0e072a211fc1b11cf1c99d6e8deb43f03183e
esp0xdeadbeef <esp0xdeadbeef@gmail.com>
2025-12-20 14:43:10 +0100

    added ppp

--- flake.nix
+++ flake.nix
@@ -114,140 +114,151 @@
     };
   };
 
-  outputs = {
-    self,
-    nixpkgs,
-    home-manager,
-    lanzaboote,
-    ...
-  } @ inputs: let
-    inherit (self) outputs;
-    # Supported systems for your flake packages, shell, etc.
-    systems = [
-      "aarch64-linux"
-      "i686-linux"
-      "x86_64-linux"
-      "aarch64-darwin"
-      "x86_64-darwin"
-    ];
-    # This is a function that generates an attribute by calling a function you
-    # pass to it, with each system as an argument
-    forAllSystems = nixpkgs.lib.genAttrs systems;
-  in {
-    # Your custom packages
-    # Accessible through 'nix build', 'nix shell', etc
-    packages = forAllSystems (system: import ./pkgs nixpkgs.legacyPackages.${system});
-    # Formatter for your nix files, available through 'nix fmt'
-    # Other options beside 'alejandra' include 'nixpkgs-fmt'
-    # formatter = forAllSystems (system: nixpkgs.legacyPackages.${system}.nixpkgs-fmt);
-    formatter = forAllSystems (system: nixpkgs.legacyPackages.${system}.nixpkgs-fmt);
+  outputs =
+    {
+      self,
+      nixpkgs,
+      home-manager,
+      lanzaboote,
+      ...
+    }@inputs:
+    let
+      inherit (self) outputs;
+      # Supported systems for your flake packages, shell, etc.
+      systems = [
+        "aarch64-linux"
+        "i686-linux"
+        "x86_64-linux"
+        "aarch64-darwin"
+        "x86_64-darwin"
+      ];
+      # This is a function that generates an attribute by calling a function you
+      # pass to it, with each system as an argument
+      forAllSystems = nixpkgs.lib.genAttrs systems;
+    in
+    {
+      # Your custom packages
+      # Accessible through 'nix build', 'nix shell', etc
+      packages = forAllSystems (system: import ./pkgs nixpkgs.legacyPackages.${system});
+      # Formatter for your nix files, available through 'nix fmt'
+      # Other options beside 'alejandra' include 'nixpkgs-fmt'
+      # formatter = forAllSystems (system: nixpkgs.legacyPackages.${system}.nixpkgs-fmt);
+      formatter = forAllSystems (system: nixpkgs.legacyPackages.${system}.nixpkgs-fmt);
 
-    # Your custom packages and modifications, exported as overlays
-    overlays = import ./overlays {inherit inputs;};
-    # Reusable nixos modules you might want to export
-    # These are usually stuff you would upstream into nixpkgs
-    nixosModules = import ./modules/nixos;
+      # Your custom packages and modifications, exported as overlays
+      overlays = import ./overlays { inherit inputs; };
+      # Reusable nixos modules you might want to export
+      # These are usually stuff you would upstream into nixpkgs
+      nixosModules = import ./modules/nixos;
 
-    # Reusable home-manager modules you might want to export
-    # These are usually stuff you would upstream into home-manager
-    homeManagerModules = import ./modules/home-manager;
+      # Reusable home-manager modules you might want to export
+      # These are usually stuff you would upstream into home-manager
+      homeManagerModules = import ./modules/home-manager;
 
-    # NixOS configuration entrypoint
-    # Available through 'nixos-rebuild --flake .#your-hostname'
-    nixosConfigurations = {
-      # test vm:
-      s-test-vm = nixpkgs.lib.nixosSystem {
-        specialArgs = {inherit inputs outputs;};
-        modules = [
-          # required for secure boot:
-          lanzaboote.nixosModules.lanzaboote
-          # > Our main nixos configuration file <
-          ./nixos/s-test-vm/configuration.nix
-        ];
-      };
-      s-test-vm-impermanence = nixpkgs.lib.nixosSystem {
-        specialArgs = {inherit inputs outputs;};
-        modules = [
-          # required for secure boot:
-          lanzaboote.nixosModules.lanzaboote
+      # NixOS configuration entrypoint
+      # Available through 'nixos-rebuild --flake .#your-hostname'
+      nixosConfigurations = {
+        # test vm:
+        s-test-vm = nixpkgs.lib.nixosSystem {
+          specialArgs = { inherit inputs outputs; };
+          modules = [
+            # required for secure boot:
+            lanzaboote.nixosModules.lanzaboote
+            # > Our main nixos configuration file <
+            ./nixos/s-test-vm/configuration.nix
+          ];
+        };
+        s-test-vm-impermanence = nixpkgs.lib.nixosSystem {
+          specialArgs = { inherit inputs outputs; };
+          modules = [
+            # required for secure boot:
+            lanzaboote.nixosModules.lanzaboote
 
-          # > Our main nixos configuration file <
-          ./nixos/s-test-vm-impermanence/configuration.nix
-        ];
-      };
-      # s-router-vpn-1 = nixpkgs.lib.nixosSystem {
-      #   specialArgs = { inherit inputs outputs; };
-      #   modules = [
-      #     # required for secure boot:
-      #     lanzaboote.nixosModules.lanzaboote
-      #     # > Our main nixos configuration file <
-      #     ./nixos/s-router-vpn-1/configuration.nix
-      #   ];
-      # };
-      s-router-impermanence = nixpkgs.lib.nixosSystem {
-        specialArgs = {inherit inputs outputs;};
-        modules = [
-          # required for secure boot:
-          lanzaboote.nixosModules.lanzaboote
-          # > Our main nixos configuration file <
-          ./nixos/s-router-impermanence/configuration.nix
-        ];
-      };
-      s-router-vpn-impermanence = nixpkgs.lib.nixosSystem {
-        specialArgs = {inherit inputs outputs;};
-        modules = [
-          # required for secure boot:
-          lanzaboote.nixosModules.lanzaboote
-          # > Our main nixos configuration file <
-          ./nixos/s-router-vpn-impermanence/configuration.nix
-        ];
-      };
+            # > Our main nixos configuration file <
+            ./nixos/s-test-vm-impermanence/configuration.nix
+          ];
+        };
+        # s-router-vpn-1 = nixpkgs.lib.nixosSystem {
+        #   specialArgs = { inherit inputs outputs; };
+        #   modules = [
+        #     # required for secure boot:
+        #     lanzaboote.nixosModules.lanzaboote
+        #     # > Our main nixos configuration file <
+        #     ./nixos/s-router-vpn-1/configuration.nix
+        #   ];
+        # };
+        s-router-impermanence = nixpkgs.lib.nixosSystem {
+          specialArgs = { inherit inputs outputs; };
+          modules = [
+            # required for secure boot:
+            lanzaboote.nixosModules.lanzaboote
+            # > Our main nixos configuration file <
+            ./nixos/s-router-impermanence/configuration.nix
+          ];
+        };
+        s-router-ppp = nixpkgs.lib.nixosSystem {
+          specialArgs = { inherit inputs outputs; };
+          modules = [
+            # required for secure boot:
+            lanzaboote.nixosModules.lanzaboote
+            # > Our main nixos configuration file <
+            ./nixos/s-router-ppp/configuration.nix
+          ];
+        };
+        s-router-vpn-impermanence = nixpkgs.lib.nixosSystem {
+          specialArgs = { inherit inputs outputs; };
+          modules = [
+            # required for secure boot:
+            lanzaboote.nixosModules.lanzaboote
+            # > Our main nixos configuration file <
+            ./nixos/s-router-vpn-impermanence/configuration.nix
+          ];
+        };
 
-
-      # x13s laptop:
-      l-x13s = nixpkgs.lib.nixosSystem {
-        specialArgs = {inherit inputs outputs;};
-        modules = [
-          # > Our main nixos configuration file <
-          ./nixos/l-x13s/configuration.nix
-        ];
-      };
-      # work laptop:
-      l-werk = nixpkgs.lib.nixosSystem {
-        specialArgs = {inherit inputs outputs;};
-        modules = [
-          lanzaboote.nixosModules.lanzaboote
-          # > Our main nixos configuration file <
-          ./nixos/l-werk/configuration.nix
-        ];
-      };
-      # private laptop:
-      l-esp = nixpkgs.lib.nixosSystem {
-        specialArgs = {inherit inputs outputs;};
-        modules = [
-          lanzaboote.nixosModules.lanzaboote
-          # > Our main nixos configuration file <
-          ./nixos/l-esp/configuration.nix
-        ];
-      };
-      # lxc server
-      s-lxc-test = nixpkgs.lib.nixosSystem {
-        specialArgs = {inherit inputs outputs;};
-        modules = [
-          # lanzaboote.nixosModules.lanzaboote
-          # > Our main nixos configuration file <
-          ./nixos/s-lxc-test/configuration.nix
-        ];
-      };
-      # lxc server
-      s-lxc-router = nixpkgs.lib.nixosSystem {
-        specialArgs = {inherit inputs outputs;};
-        modules = [
-          # lanzaboote.nixosModules.lanzaboote
-          # > Our main nixos configuration file <
-          ./nixos/s-lxc-router/configuration.nix
-        ];
+        # x13s laptop:
+        l-x13s = nixpkgs.lib.nixosSystem {
+          specialArgs = { inherit inputs outputs; };
+          modules = [
+            # > Our main nixos configuration file <
+            ./nixos/l-x13s/configuration.nix
+          ];
+        };
+        # work laptop:
+        l-werk = nixpkgs.lib.nixosSystem {
+          specialArgs = { inherit inputs outputs; };
+          modules = [
+            lanzaboote.nixosModules.lanzaboote
+            # > Our main nixos configuration file <
+            ./nixos/l-werk/configuration.nix
+          ];
+        };
+        # private laptop:
+        l-esp = nixpkgs.lib.nixosSystem {
+          specialArgs = { inherit inputs outputs; };
+          modules = [
+            lanzaboote.nixosModules.lanzaboote
+            # > Our main nixos configuration file <
+            ./nixos/l-esp/configuration.nix
+          ];
+        };
+        # lxc server
+        s-lxc-test = nixpkgs.lib.nixosSystem {
+          specialArgs = { inherit inputs outputs; };
+          modules = [
+            # lanzaboote.nixosModules.lanzaboote
+            # > Our main nixos configuration file <
+            ./nixos/s-lxc-test/configuration.nix
+          ];
+        };
+        # lxc server
+        s-lxc-router = nixpkgs.lib.nixosSystem {
+          specialArgs = { inherit inputs outputs; };
+          modules = [
+            # lanzaboote.nixosModules.lanzaboote
+            # > Our main nixos configuration file <
+            ./nixos/s-lxc-router/configuration.nix
+          ];
+        };
       };
     };
-  };
 }
