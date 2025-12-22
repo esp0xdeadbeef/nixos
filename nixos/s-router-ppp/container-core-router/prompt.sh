901d70da8c93521a2c6866726bfa02f00b99fb7a
esp0xdeadbeef <esp0xdeadbeef@gmail.com>
2025-12-22 05:21:02 +0100

    changed prompt

--- nixos/s-router-ppp/container-core-router/prompt.sh
+++ nixos/s-router-ppp/container-core-router/prompt.sh
@@ -2,7 +2,7 @@
 
 echo -e '“pppoe-wan-to-downstream should:\nreceive a /48 from the ISP\nuse one /64 from it for its own LAN\nroute the rest” but it does not, ipv4 AND ipv6 should be working, this is my config, fix it, check the imports in !!configuration.nix!!: ' ;
 
-find . -type f -exec sh -c '
+find . -type f -not -name '*.sh' -exec sh -c '
   echo
   echo "### FILE: $1 ###"
   cat "$1"
@@ -13,3 +13,4 @@ ssh s-router-ppp sudo nixos-container run pppoe-wan-to-downstream -- journalctl 
 echo this is my ip a output at this moment:
 ssh s-router-ppp sudo nixos-container run pppoe-wan-to-downstream -- ip a
 
+ssh s-router-ppp sudo nixos-container run pppoe-wan-to-downstream -- bash -c 'ping $(dig google.com @1.1.1.1 +short AAAA)'
