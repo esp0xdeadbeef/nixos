619ba3141c9333be66a310cf655928c67189147c
esp0xdeadbeef <esp0xdeadbeef@gmail.com>
2026-01-10 13:09:34 +0100

    added clevis to not use dhcp, router race contidition.

--- nixos/s-sigma/hardware/disks/clevis.nix
+++ nixos/s-sigma/hardware/disks/clevis.nix
@@ -10,16 +10,29 @@
   boot.initrd.clevis.enable = true;
   boot.initrd.clevis.useTang = true;
   boot.initrd.clevis.devices."crypted".secretFile = ./nvme0n1p1.jwe;
- 
+
   boot.initrd.systemd.network.enable = true;
   boot.initrd.systemd.network.wait-online.enable = true;
   boot.initrd.systemd.network.wait-online.timeout = 30;
- 
+
   boot.initrd.systemd.network.networks."10-eno4" = {
-    matchConfig.Name = "eno4";
-    networkConfig.DHCP = "yes";
+    matchConfig = {
+      Name = "eno4";
+    };
+    networkConfig = {
+      Address = "192.168.1.98/24";
+    };
   };
 
-  boot.initrd.kernelModules = [ "bnx2" "bnx2x" "ixgbe" ];
-}
+  # this server (s-sigma) will start my router, this will lead to raceconditions with DHCP:
+  #boot.initrd.systemd.network.networks."10-eno4" = {
+  #  matchConfig.Name = "eno4";
+  #  networkConfig.DHCP = "yes";
+  #};
 
+  boot.initrd.kernelModules = [
+    "bnx2"
+    "bnx2x"
+    "ixgbe"
+  ];
+}
