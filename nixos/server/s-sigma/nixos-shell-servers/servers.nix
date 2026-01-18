f384e54a8be42227277b84e268e89ec2cbcb2f82
esp0xdeadbeef <esp0xdeadbeef@gmail.com>
2026-01-18 01:44:40 +0100

    added server flag, remote or local, local for now, testing still.

--- nixos/server/s-sigma/nixos-shell-servers/servers.nix
+++ nixos/server/s-sigma/nixos-shell-servers/servers.nix
@@ -7,38 +7,49 @@
 
 let
   mkVM = import ./mk-nixos-shell-vm.nix { inherit pkgs lib self; };
+
+  useLocalRepo = true;
+
+  repoArg =
+    if useLocalRepo
+    then "path:/home/deadbeef/github/nixos"
+    else null;
+
+  withRepo = attrs:
+    if repoArg == null
+    then builtins.removeAttrs attrs [ "repository" ]
+    else attrs // { repository = repoArg; };
+
 in
 {
   config = lib.mkMerge [
-    (mkVM "s-infra" {
+    (mkVM "s-infra" (withRepo {
       description = "Infra VM (nixos-shell)";
       keep = 1;
       extraTmpfiles = [
         "d /persist/infra/unifi 0755 root root -"
       ];
-      # repository = "path:/home/deadbeef/github/nixos";
-    })
-    (mkVM "s-sigma-s-router-edge" {
+    }))
+
+    (mkVM "s-sigma-s-router-edge" (withRepo {
       description = "s-router-edge VM (nixos-shell)";
       keep = 1;
-      #repository = "path:/home/deadbeef/github/nixos";
-    })
+    }))
 
-    (mkVM "s-gameservers" {
+    (mkVM "s-gameservers" (withRepo {
       description = "Gameserver VM (nixos-shell)";
       keep = 1;
-      #repository = "path:/home/deadbeef/github/nixos";
-    })
-    (mkVM "s-sigma-s-router-vpn-egress" {
+    }))
+
+    (mkVM "s-sigma-s-router-vpn-egress" (withRepo {
       description = "VPN-egress VM (nixos-shell)";
       keep = 1;
-      #repository = "path:/home/deadbeef/github/nixos";
-    })
+    }))
 
-    (mkVM "s-test" {
+    (mkVM "s-test" (withRepo {
       description = "s-test (nixos-shell)";
       keep = 2;
-      #repository = "path:/home/deadbeef/github/nixos";
-    })
+    }))
   ];
 }
+
