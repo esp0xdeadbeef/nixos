c95279e2d0c864c56797d30de6aef94952a4eff7
esp0xdeadbeef <esp0xdeadbeef@gmail.com>
2026-01-27 00:26:56 +0100

    added prune for older generations (old boots and profiles)

--- library/01-general/system/garbage-collection.nix
+++ library/01-general/system/garbage-collection.nix
@@ -7,4 +7,25 @@
     options = "--delete-older-than 30d";
     randomizedDelaySec = "5min";
   };
+
+  systemd.services.nix-prune-generations = {
+    description = "Prune old NixOS system generations";
+    serviceConfig = {
+      Type = "oneshot";
+      ExecStart = ''
+        ${pkgs.nix}/bin/nix-env \
+          --profile /nix/var/nix/profiles/system \
+          --delete-generations +30
+      '';
+    };
+  };
+
+  systemd.timers.nix-prune-generations = {
+    wantedBy = [ "timers.target" ];
+    timerConfig = {
+      OnCalendar = "weekly";
+      Persistent = true;
+    };
+  };
+
 }
