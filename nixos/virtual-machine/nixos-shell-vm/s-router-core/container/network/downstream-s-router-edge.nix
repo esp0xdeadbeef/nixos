4bbc39fa475932578ceeb80ec87aeb4cc458e80d
esp0xdeadbeef <esp0xdeadbeef@gmail.com>
2026-01-31 04:36:05 +0100

    working vpn-egress, routes added to core

--- nixos/virtual-machine/nixos-shell-vm/s-router-core/container/network/downstream-s-router-edge.nix
+++ nixos/virtual-machine/nixos-shell-vm/s-router-core/container/network/downstream-s-router-edge.nix
@@ -14,6 +14,20 @@
     addresses = [
       { Address = "10.255.255.1/29"; }
     ];
+    routes = [
+      {
+        routeConfig = {
+          Destination = "10.13.37.0/24";
+          Gateway = "10.255.255.2";
+        };
+      }
+      {
+        routeConfig = {
+          Destination = "10.10.3.100/24";
+          Gateway = "10.255.255.2";
+        };
+      }
+    ];
+
   };
 }
-
