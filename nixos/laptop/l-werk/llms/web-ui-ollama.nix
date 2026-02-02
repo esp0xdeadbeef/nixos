073696bd1e86619a34697f6acbf59059ea8fe045
esp0xdeadbeef-l-werk <esp0xdeadbeef@gmail.com>
2026-02-03 00:27:19 +0100

    added webui for ollama

---
+++ nixos/laptop/l-werk/llms/web-ui-ollama.nix
@@ -0,0 +1,13 @@
+{ config, pkgs, ... }:
+
+{
+virtualisation.oci-containers.containers.open-webui = {
+  image = "ghcr.io/open-webui/open-webui";
+  autoStart = true;
+  ports = [ "127.0.0.1:3000:8080" ];
+  environment = {
+    OLLAMA_BASE_URL = "http://host.containers.internal:11434";
+  };
+};
+
+}
