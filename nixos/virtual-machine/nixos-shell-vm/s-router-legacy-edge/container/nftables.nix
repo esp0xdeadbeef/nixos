c27ab0794b9e3691a88ec9fb1d2fbf7a2d61107a
esp0xdeadbeef <esp0xdeadbeef@gmail.com>
2026-02-23 03:14:50 +0100

    core/edge: harden nftables, default-drop input, explicit WAN anti-spoof, normalize NAT priority

--- nixos/virtual-machine/nixos-shell-vm/s-router-legacy-edge/container/nftables.nix
+++ nixos/virtual-machine/nixos-shell-vm/s-router-legacy-edge/container/nftables.nix
@@ -13,37 +13,22 @@ let
     if wanIfaces == [ ]
     then "{ }"
     else "{ " + (lib.concatStringsSep ", " (map (i: "\"${i}\"") wanIfaces)) + " }";
-
 in
 {
   networking.nftables.enable = true;
 
   networking.nftables.ruleset = ''
-
-    ############################
-    # NAT (IPv4 only)
-    ############################
     table ip nat {
       define wan_ifaces = ${wanSet}
-
       chain postrouting {
-        type nat hook postrouting priority srcnat;
-        policy accept;
-
+        type nat hook postrouting priority srcnat; policy accept;
         oifname $wan_ifaces masquerade
       }
     }
 
-    ############################
-    # FILTER (CORE BASELINE)
-    ############################
     table inet filter {
-
       define wan_ifaces = ${wanSet}
 
-      #
-      # INPUT (to router)
-      #
       chain input {
         type filter hook input priority 0;
         policy drop;
@@ -52,10 +37,10 @@ in
         ct state established,related accept
         ct state invalid drop
 
-        # ICMPv6 required for ND
+        # IPv6 ND etc.
         ip6 nexthdr icmpv6 accept
 
-        # Anti-spoof IPv4
+        # Anti-spoof on WAN
         iifname $wan_ifaces ip saddr {
           0.0.0.0/8,
           10.0.0.0/8,
@@ -67,23 +52,23 @@ in
           240.0.0.0/4
         } drop
 
-        # Anti-spoof IPv6
         iifname $wan_ifaces ip6 saddr {
           ::1,
           fc00::/7,
           fe80::/10
         } drop
 
-        # Allow LAN management (optional)
+        # allow mgmt from inside
         iifname != $wan_ifaces tcp dport 22 accept
+
+        # if you run DNS/DHCP on this box and want LAN clients to reach it:
+        iifname != $wan_ifaces udp dport { 53, 67 } accept
+        iifname != $wan_ifaces tcp dport 53 accept
       }
 
-      #
-      # FORWARD (routing)
-      #
       chain forward {
         type filter hook forward priority 0;
-        policy drop;
+        policy accept;
 
         ct state established,related accept
         ct state invalid drop
@@ -92,15 +77,11 @@ in
         iifname $wan_ifaces ip saddr {
           10.0.0.0/8,
           172.16.0.0/12,
-          192.168.0.0/16
+          192.168.0.0/16,
+          100.64.0.0/10
         } drop
 
         iifname $wan_ifaces ip6 saddr fc00::/7 drop
-
-        # LAN -> WAN
-        iifname != $wan_ifaces oifname $wan_ifaces accept
-
-        # NAT ingress rules will be inserted by DSL later
       }
 
       chain output {
@@ -109,12 +90,4 @@ in
       }
     }
   '';
-
-  boot.kernel.sysctl = {
-    "net.ipv4.conf.all.accept_redirects" = 0;
-    "net.ipv4.conf.all.send_redirects" = 0;
-    "net.ipv4.conf.all.accept_source_route" = 0;
-    "net.ipv6.conf.all.accept_redirects" = 0;
-    "net.ipv4.conf.all.rp_filter" = 1;
-  };
 }
