617e207f684ba0476415db66a2bbbf108513522b
esp0xdeadbeef <esp0xdeadbeef@gmail.com>
2026-03-23 23:42:23 +0100

    removed kitty and hopefully made darkmode better

--- home-manager/01-general/darkmode/config.nix
+++ home-manager/01-general/darkmode/config.nix
@@ -1,38 +1,49 @@
-{ config, pkgs, ... }:
+{ pkgs, ... }:
 
 {
-  # Set GTK dark theme
   gtk = {
     enable = true;
+
     theme = {
       name = "Adwaita-dark";
       package = pkgs.gnome-themes-extra;
     };
+
     iconTheme = {
       name = "Papirus-Dark";
       package = pkgs.papirus-icon-theme;
     };
+
+    # Default preference: dark
+    colorScheme = "dark";
+
+    # Do not force GTK4 theme through the Home Manager workaround
+    gtk4.theme = null;
   };
 
-  # Set QT theme to dark
   qt = {
     enable = true;
-    platformTheme.name = "gtk"; # Or "qt5ct" if you prefer
-    style.name = "Adwaita-dark";
+
+    # Avoid current HM "gtk" -> "gtk2" weirdness.
+    # "gtk3" is the safer choice right now.
+    platformTheme.name = "gtk3";
+
+    # Uncomment only if you want to FORCE Qt dark styling,
+    # instead of just expressing the system preference.
+    style.name = "adwaita-dark";
   };
 
-  # Export environment variable for your own Electron apps
-  home.sessionVariables = {
-    DARKMODE = "1";
-    # didn't work, still white mode default config:
-    _JAVA_OPTIONS = "-Dswing.defaultlaf=com.formdev.flatlaf.FlatDarculaLaf";
-  };
-
-  # Set GSettings to prefer dark mode (important for some apps like Electron using portals)
   dconf.settings = {
     "org/gnome/desktop/interface" = {
       color-scheme = "prefer-dark";
       gtk-theme = "Adwaita-dark";
+      icon-theme = "Papirus-Dark";
     };
+  };
+
+  home.sessionVariables = {
+    # Not needed for the clean/default path:
+    DARKMODE = "1";
+    _JAVA_OPTIONS = "-Dswing.defaultlaf=com.formdev.flatlaf.FlatDarculaLaf";
   };
 }
