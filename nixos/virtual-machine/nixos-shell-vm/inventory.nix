401fc99d2ee2a93bbc18463e705d34f7813bc6d9
esp0xdeadbeef <esp0xdeadbeef@gmail.com>
2026-04-08 20:55:20 +0200

    added downstream selector

--- nixos/virtual-machine/nixos-shell-vm/inventory.nix
+++ nixos/virtual-machine/nixos-shell-vm/inventory.nix
@@ -1,6 +1,18 @@
 {
   schemaVersion = 1;
 
+  endpoints = {
+    web01 = {
+      ipv4 = [ "10.20.15.10" ];
+      ipv6 = [ "fd42:dead:beef:15::10" ];
+    };
+
+    s-sigma = {
+      ipv4 = [ "10.20.10.10" ];
+      ipv6 = [ "fd42:dead:beef:10::10" ];
+    };
+  };
+
   deployment = {
     hosts = {
       s-router-access = {
@@ -199,8 +211,8 @@
 
         logicalNode = {
           enterprise = "esp0xdeadbeef";
-          name = "s-router-core-wan";
           site = "site-a";
+          name = "s-router-core-wan";
         };
 
         ports = {
@@ -216,7 +228,8 @@
           };
 
           wan = {
-            uplink = "wan";
+            upstream = "upstream-core";
+            link = "wan";
             attach = {
               kind = "bridge";
               bridge = "br-upstream";
@@ -228,14 +241,14 @@
         };
       };
 
-      s-router-access-admin = {
+      esp0xdeadbeef-site-a-s-router-access-admin = {
         host = "s-router-access";
         platform = "linux";
 
         logicalNode = {
           enterprise = "esp0xdeadbeef";
-          name = "s-router-access-admin";
           site = "site-a";
+          name = "s-router-access-admin";
         };
 
         ports = {
@@ -261,16 +274,42 @@
             };
           };
         };
+
+        advertisements = {
+          dhcp4 = {
+            tenant-admin = {
+              interface = "tenant-admin";
+              id = "tenant-admin";
+              subnet = "10.20.15.0/24";
+              pool = {
+                start = "10.20.15.100";
+                end = "10.20.15.200";
+              };
+              router = "10.20.15.1";
+              dnsServers = [ "10.20.15.1" ];
+              domain = "lan.";
+            };
+          };
+
+          ipv6Ra = {
+            tenant-admin = {
+              interface = "tenant-admin";
+              prefixes = [ "fd42:dead:beef:15::/64" ];
+              rdnss = [ "fd42:dead:beef:15::1" ];
+              dnssl = [ "lan." ];
+            };
+          };
+        };
       };
 
-      s-router-access-client = {
+      esp0xdeadbeef-site-a-s-router-access-client = {
         host = "s-router-access";
         platform = "linux";
 
         logicalNode = {
           enterprise = "esp0xdeadbeef";
-          name = "s-router-access-client";
           site = "site-a";
+          name = "s-router-access-client";
         };
 
         ports = {
@@ -296,16 +335,42 @@
             };
           };
         };
+
+        advertisements = {
+          dhcp4 = {
+            tenant-client = {
+              interface = "tenant-client";
+              id = "tenant-client";
+              subnet = "10.20.20.0/24";
+              pool = {
+                start = "10.20.20.100";
+                end = "10.20.20.200";
+              };
+              router = "10.20.20.1";
+              dnsServers = [ "10.20.20.1" ];
+              domain = "lan.";
+            };
+          };
+
+          ipv6Ra = {
+            tenant-client = {
+              interface = "tenant-client";
+              prefixes = [ "fd42:dead:beef:20::/64" ];
+              rdnss = [ "fd42:dead:beef:20::1" ];
+              dnssl = [ "lan." ];
+            };
+          };
+        };
       };
 
-      s-router-access-mgmt = {
+      esp0xdeadbeef-site-a-s-router-access-mgmt = {
         host = "s-router-access";
         platform = "linux";
 
         logicalNode = {
           enterprise = "esp0xdeadbeef";
-          name = "s-router-access-mgmt";
           site = "site-a";
+          name = "s-router-access-mgmt";
         };
 
         ports = {
@@ -331,16 +396,42 @@
             };
           };
         };
+
+        advertisements = {
+          dhcp4 = {
+            tenant-mgmt = {
+              interface = "tenant-mgmt";
+              id = "tenant-mgmt";
+              subnet = "10.20.10.0/24";
+              pool = {
+                start = "10.20.10.100";
+                end = "10.20.10.200";
+              };
+              router = "10.20.10.1";
+              dnsServers = [ "10.20.10.1" ];
+              domain = "lan.";
+            };
+          };
+
+          ipv6Ra = {
+            tenant-mgmt = {
+              interface = "tenant-mgmt";
+              prefixes = [ "fd42:dead:beef:10::/64" ];
+              rdnss = [ "fd42:dead:beef:10::1" ];
+              dnssl = [ "lan." ];
+            };
+          };
+        };
       };
 
-      s-router-policy-only = {
+      esp0xdeadbeef-site-a-s-router-policy = {
         host = "s-router-policy";
         platform = "nixos-container";
 
         logicalNode = {
           enterprise = "esp0xdeadbeef";
-          name = "s-router-policy";
           site = "site-a";
+          name = "s-router-policy";
         };
 
         ports = {
@@ -368,14 +459,14 @@
         };
       };
 
-      s-router-upstream-selector = {
+      esp0xdeadbeef-site-a-s-router-upstream-selector = {
         host = "s-router-policy";
-        platform = "linux";
+        platform = "nixos-container";
 
         logicalNode = {
           enterprise = "esp0xdeadbeef";
-          name = "s-router-upstream-selector";
           site = "site-a";
+          name = "s-router-upstream-selector";
         };
 
         ports = {
@@ -403,14 +494,14 @@
         };
       };
 
-      s-router-downstream-selector = {
+      esp0xdeadbeef-site-a-s-router-downstream-selector = {
         host = "s-router-policy";
-        platform = "linux";
+        platform = "nixos-container";
 
         logicalNode = {
           enterprise = "esp0xdeadbeef";
-          name = "s-router-downstream-selector";
           site = "site-a";
+          name = "s-router-downstream-selector";
         };
 
         ports = {
@@ -421,7 +512,7 @@
               bridge = "tr202";
             };
             interface = {
-              name = "policy"
+              name = "policy";
             };
           };
 
@@ -479,7 +570,7 @@
         deploymentHost = "s-router-core";
       };
 
-      s-router-policy-only = {
+      s-router-policy = {
         containerName = "s-router-policy-only-container";
         deploymentHost = "s-router-policy";
       };
--- nixos/virtual-machine/nixos-shell-vm/s-router-test/inventory.nix
+++ nixos/virtual-machine/nixos-shell-vm/s-router-test/inventory.nix
@@ -1,6 +1,18 @@
 {
   schemaVersion = 1;
 
+  endpoints = {
+    web01 = {
+      ipv4 = [ "10.20.15.10" ];
+      ipv6 = [ "fd42:dead:beef:15::10" ];
+    };
+
+    s-sigma = {
+      ipv4 = [ "10.20.10.10" ];
+      ipv6 = [ "fd42:dead:beef:10::10" ];
+    };
+  };
+
   deployment = {
     hosts = {
       s-router-test = {
@@ -78,6 +90,12 @@
             parentUplink = "trunk";
           };
 
+          tr103 = {
+            name = "tr103";
+            vlan = 403;
+            parentUplink = "trunk";
+          };
+
           tr200 = {
             name = "tr200";
             vlan = 500;
@@ -120,22 +138,11 @@
 
         logicalNode = {
           enterprise = "esp0xdeadbeef";
-          name = "s-router-core-wan";
           site = "site-a";
+          name = "s-router-core-wan";
         };
 
         ports = {
-          wan = {
-            uplink = "wan";
-            attach = {
-              kind = "bridge";
-              bridge = "br-upstream";
-            };
-            interface = {
-              name = "wan";
-            };
-          };
-
           upstream-selector = {
             link = "p2p-s-router-core-wan-s-router-upstream-selector";
             attach = {
@@ -146,22 +153,34 @@
               name = "ens3";
             };
           };
+
+          wan = {
+            upstream = "upstream-core";
+            link = "wan";
+            attach = {
+              kind = "bridge";
+              bridge = "br-upstream";
+            };
+            interface = {
+              name = "wan";
+            };
+          };
         };
       };
 
-      s-router-access-admin = {
+      esp0xdeadbeef-site-a-s-router-access-admin = {
         host = "s-router-test";
         platform = "linux";
 
         logicalNode = {
           enterprise = "esp0xdeadbeef";
-          name = "s-router-access-admin";
           site = "site-a";
+          name = "s-router-access-admin";
         };
 
         ports = {
-          transit-policy = {
-            link = "p2p-s-router-access-admin-s-router-policy";
+          transit-downstream-selector = {
+            link = "p2p-s-router-access-admin-s-router-downstream-selector";
             attach = {
               kind = "bridge";
               bridge = "tr100";
@@ -182,21 +201,47 @@
             };
           };
         };
+
+        advertisements = {
+          dhcp4 = {
+            tenant-admin = {
+              interface = "tenant-admin";
+              id = "tenant-admin";
+              subnet = "10.20.15.0/24";
+              pool = {
+                start = "10.20.15.100";
+                end = "10.20.15.200";
+              };
+              router = "10.20.15.1";
+              dnsServers = [ "10.20.15.1" ];
+              domain = "lan.";
+            };
+          };
+
+          ipv6Ra = {
+            tenant-admin = {
+              interface = "tenant-admin";
+              prefixes = [ "fd42:dead:beef:15::/64" ];
+              rdnss = [ "fd42:dead:beef:15::1" ];
+              dnssl = [ "lan." ];
+            };
+          };
+        };
       };
 
-      s-router-access-client = {
+      esp0xdeadbeef-site-a-s-router-access-client = {
         host = "s-router-test";
         platform = "linux";
 
         logicalNode = {
           enterprise = "esp0xdeadbeef";
-          name = "s-router-access-client";
           site = "site-a";
+          name = "s-router-access-client";
         };
 
         ports = {
-          transit-policy = {
-            link = "p2p-s-router-access-client-s-router-policy";
+          transit-downstream-selector = {
+            link = "p2p-s-router-access-client-s-router-downstream-selector";
             attach = {
               kind = "bridge";
               bridge = "tr102";
@@ -217,21 +262,47 @@
             };
           };
         };
+
+        advertisements = {
+          dhcp4 = {
+            tenant-client = {
+              interface = "tenant-client";
+              id = "tenant-client";
+              subnet = "10.20.20.0/24";
+              pool = {
+                start = "10.20.20.100";
+                end = "10.20.20.200";
+              };
+              router = "10.20.20.1";
+              dnsServers = [ "10.20.20.1" ];
+              domain = "lan.";
+            };
+          };
+
+          ipv6Ra = {
+            tenant-client = {
+              interface = "tenant-client";
+              prefixes = [ "fd42:dead:beef:20::/64" ];
+              rdnss = [ "fd42:dead:beef:20::1" ];
+              dnssl = [ "lan." ];
+            };
+          };
+        };
       };
 
-      s-router-access-mgmt = {
+      esp0xdeadbeef-site-a-s-router-access-mgmt = {
         host = "s-router-test";
         platform = "linux";
 
         logicalNode = {
           enterprise = "esp0xdeadbeef";
-          name = "s-router-access-mgmt";
           site = "site-a";
+          name = "s-router-access-mgmt";
         };
 
         ports = {
-          transit-policy = {
-            link = "p2p-s-router-access-mgmt-s-router-policy";
+          transit-downstream-selector = {
+            link = "p2p-s-router-access-mgmt-s-router-downstream-selector";
             attach = {
               kind = "bridge";
               bridge = "tr101";
@@ -252,52 +323,102 @@
             };
           };
         };
+
+        advertisements = {
+          dhcp4 = {
+            tenant-mgmt = {
+              interface = "tenant-mgmt";
+              id = "tenant-mgmt";
+              subnet = "10.20.10.0/24";
+              pool = {
+                start = "10.20.10.100";
+                end = "10.20.10.200";
+              };
+              router = "10.20.10.1";
+              dnsServers = [ "10.20.10.1" ];
+              domain = "lan.";
+            };
+          };
+
+          ipv6Ra = {
+            tenant-mgmt = {
+              interface = "tenant-mgmt";
+              prefixes = [ "fd42:dead:beef:10::/64" ];
+              rdnss = [ "fd42:dead:beef:10::1" ];
+              dnssl = [ "lan." ];
+            };
+          };
+        };
       };
 
-      s-router-policy-only = {
+      esp0xdeadbeef-site-a-s-router-downstream-selector = {
         host = "s-router-test";
-        platform = "nixos-container";
+        platform = "linux";
 
         logicalNode = {
           enterprise = "esp0xdeadbeef";
-          name = "s-router-policy";
           site = "site-a";
+          name = "s-router-downstream-selector";
         };
 
         ports = {
-          transit-admin = {
-            link = "p2p-s-router-access-admin-s-router-policy";
+          policy = {
+            link = "p2p-s-router-downstream-selector-s-router-policy";
+            attach = {
+              kind = "bridge";
+              bridge = "tr103";
+            };
+            interface = {
+              name = "policy";
+            };
+          };
+
+          access-admin = {
+            link = "p2p-s-router-access-admin-s-router-downstream-selector";
             attach = {
               kind = "bridge";
               bridge = "tr100";
             };
             interface = {
-              name = "transit-admin";
+              name = "access-admin";
             };
           };
 
-          transit-client = {
-            link = "p2p-s-router-access-client-s-router-policy";
+          access-client = {
+            link = "p2p-s-router-access-client-s-router-downstream-selector";
             attach = {
               kind = "bridge";
               bridge = "tr102";
             };
             interface = {
-              name = "transit-client";
+              name = "access-client";
             };
           };
 
-          transit-mgmt = {
-            link = "p2p-s-router-access-mgmt-s-router-policy";
+          access-mgmt = {
+            link = "p2p-s-router-access-mgmt-s-router-downstream-selector";
             attach = {
               kind = "bridge";
               bridge = "tr101";
             };
             interface = {
-              name = "transit-mgmt";
+              name = "access-mgmt";
             };
           };
+        };
+      };
 
+      esp0xdeadbeef-site-a-s-router-policy = {
+        host = "s-router-test";
+        platform = "nixos-container";
+
+        logicalNode = {
+          enterprise = "esp0xdeadbeef";
+          site = "site-a";
+          name = "s-router-policy";
+        };
+
+        ports = {
           upstream-selector = {
             link = "p2p-s-router-policy-s-router-upstream-selector";
             attach = {
@@ -308,17 +429,28 @@
               name = "upstream";
             };
           };
+
+          downstream-selector = {
+            link = "p2p-s-router-downstream-selector-s-router-policy";
+            attach = {
+              kind = "bridge";
+              bridge = "tr103";
+            };
+            interface = {
+              name = "downstream";
+            };
+          };
         };
       };
 
-      s-router-upstream-selector = {
+      esp0xdeadbeef-site-a-s-router-upstream-selector = {
         host = "s-router-test";
         platform = "linux";
 
         logicalNode = {
           enterprise = "esp0xdeadbeef";
-          name = "s-router-upstream-selector";
           site = "site-a";
+          name = "s-router-upstream-selector";
         };
 
         ports = {
@@ -365,8 +497,11 @@
         deploymentHost = "s-router-test";
       };
 
-      s-router-policy-only = {
-        containerName = "s-router-policy-only-container";
+      s-router-downstream-selector = {
+        deploymentHost = "s-router-test";
+      };
+
+      s-router-policy = {
         deploymentHost = "s-router-test";
       };
 
