52b22ba27d051e25f20ffa9379b81bf93f005424
esp0xdeadbeef <esp0xdeadbeef@gmail.com>
2026-05-01 16:38:28 +0200

    following the hardware for x13s also

--- nixos/laptop/l-x13s/default.nix
+++ nixos/laptop/l-x13s/default.nix
@@ -1,5 +1,3 @@
-# This is your system's configuration file.
-# Use this to configure your system environment (it replaces /etc/nixos/configuration.nix)
 {
   inputs,
   outputs,
@@ -9,80 +7,45 @@
   ...
 }:
 {
-  # You can import other NixOS modules here
   imports = [
-    # If you want to use modules your own flake exports (from modules/nixos):
-    # outputs.nixosModules.example
-
-    # Or modules from other flakes (such as nixos-hardware):
-    # inputs.hardware.nixosModules.common-cpu-intel
-    # inputs.nixos-hardware.nixosModules.common-ssd
-
-    # You can also split up your configuration and import pieces of it here:
-    # ./users.nix
-    # Import your generated (nixos-generate-config) hardware configuration
-
-    # cd /home/deadbeef/github/nixos/nixos/l-x13s ; ./generate-imports.sh
     ./hardware/bootloader.nix
     ./hardware/hardware-configuration.nix
     ./packages/packages.nix
     ./packages/widevine.nix
 
-    #../../../library/01-general
-
-    inputs.hardware.nixosModules.lenovo-thinkpad-x13s
+    # X13s-only nixos-hardware branch:
+    # https://github.com/NixOS/nixos-hardware/pull/1751
+    inputs.nixos-hardware-x13s.nixosModules.lenovo-thinkpad-x13s
 
     inputs.home-manager.nixosModules.home-manager
-    # inputs.nixos-x13s.nixosModules.default
 
-    # (import "${inputs.home-manager}")
-    # {inputs.home-manager.users.deadbeef = import ./home-manager/l-werk/home.nix;}
-    # inputs.home-manager.nixosModules.home-manager
+    # inputs.nixos-x13s.nixosModules.default
   ];
 
-  # inputs.hardware.nixosModules.lenovo-thinkpad-x13s = {
-  #   enable = true;
-  #   bluetoothMac = "E9:1C:3B:F0:FD:8C";
-  #   wifiMac = "8C:FD:F0:1C:3B:0A";
-  #   kernel = "mainline";
-  # };
-
-  # inputs.nixos-x13s.nixosModules.default
-
   home-manager = {
-    extraSpecialArgs = { inherit inputs outputs; };
+    extraSpecialArgs = {
+      inherit inputs outputs;
+    };
+
     users = {
-      # Import your home-manager configuration
       deadbeef = import ../../../home-manager/l-x13s/home.nix;
     };
   };
+
   nixpkgs = {
-    # You can add overlays here
     overlays = [
-      # Add overlays your own flake exports (from overlays and pkgs dir):
       outputs.overlays.additions
       outputs.overlays.modifications
       outputs.overlays.unstable-packages
 
-      # You can also add overlays exported from other flakes:
-      # neovim-nightly-overlay.overlays.default
-
-      # Or define it inline, for example:
-      # (final: prev: {
-      #   hi = final.hello.overrideAttrs (oldAttrs: {
-      #     patches = [ ./change-hello-to-hi.patch ];
-      #   });
-      # })
-
-      # widevine patch:
+      # Widevine patch.
       inputs.nixos-aarch64-widevine.overlays.default
-
     ];
-    # Configure your nixpkgs instance
+
     config = {
-      # Disable if you don't want unfree packages
       allowUnfree = true;
     };
+
     hostPlatform = "aarch64-linux";
   };
 
@@ -92,55 +55,50 @@
     in
     {
       settings = {
-        # Enable flakes and new 'nix' command
         experimental-features = "nix-command flakes";
-        # Opinionated: disable global registry
+
+        # Opinionated: disable global registry.
         flake-registry = "";
-        # Workaround for https://github.com/NixOS/nix/issues/9574
+
+        # Workaround for:
+        # https://github.com/NixOS/nix/issues/9574
         nix-path = config.nix.nixPath;
       };
-      # Opinionated: disable channels
+
+      # Opinionated: disable channels.
       channel.enable = false;
 
-      # Opinionated: make flake registry and nix path match flake inputs
+      # Opinionated: make flake registry and nix path match flake inputs.
       registry = lib.mapAttrs (_: flake: { inherit flake; }) flakeInputs;
       nixPath = lib.mapAttrsToList (n: _: "${n}=flake:${n}") flakeInputs;
     };
 
-  # FIXME: Add the rest of your current configuration
   networking.hostName = "l-x13s";
   networking.networkmanager.enable = true;
 
   users.users = {
-    # FIXME: Replace with your username
     deadbeef = {
-      # TODO: You can set an initial password for your user.
-      # If you do, you can skip setting a root password by passing '--no-root-passwd' to nixos-install.
-      # Be sure to change it (using passwd) after rebooting!
       initialPassword = " ";
       isNormalUser = true;
+
       openssh.authorizedKeys.keys = [
-        # TODO: Add your SSH public key(s) here, if you plan on using SSH to connect
         "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIBKIjWf+YcfijNBH+ilujFPNpgVZH9jD1PA1GiIzIWxO deadbeef@l-x13s"
       ];
-      # TODO: Be sure to add any other groups you need (such as networkmanager, audio, docker, etc)
-      extraGroups = [ "wheel" ];
+
+      extraGroups = [
+        "wheel"
+      ];
     };
   };
 
-  # This setups a SSH server. Very important if you're setting up a headless system.
-  # Feel free to remove if you don't need it.
   services.openssh = {
     enable = true;
+
     settings = {
-      # Opinionated: forbid root login through SSH.
       PermitRootLogin = "no";
-      # Opinionated: use keys only.
-      # Remove if you want to SSH using passwords
       PasswordAuthentication = true;
     };
   };
 
-  # https://nixos.wiki/wiki/FAQ/When_do_I_update_stateVersion
   system.stateVersion = "24.11";
 }
