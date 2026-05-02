bad13c0c61051a5d6425b20b3fcab4a89ff80221
esp0xdeadbeef <esp0xdeadbeef@gmail.com>
2026-05-03 01:23:40 +0200

    Refresh router test SSH key and encrypted secret metadata

--- nixos/virtual-machine/nixos-anywhere/s-router-hetzner-anywhere/runtime.nix
+++ nixos/virtual-machine/nixos-anywhere/s-router-hetzner-anywhere/runtime.nix
@@ -7,7 +7,7 @@
   ];
   authorizedKeys = [
     "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIAqEmMbztRhj2zE1dXf5Z+Ow7mXXXE6sNAG4/hrIOrmD deadbeef@codex-jail"
-    "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIKLLxXh18rihWnA/X7HMzm7dMW8eImnpWKfXZm6h1+ne root@s-router-test"
+    "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIOdAWOlRk8eN2/rb7jJMCKrI/YW4FQMlQs1nfr86TS7p root@s-router-test"
   ];
   primaryInterface = "ens3";
   primaryInterfaceMatch = "en* eth0";
