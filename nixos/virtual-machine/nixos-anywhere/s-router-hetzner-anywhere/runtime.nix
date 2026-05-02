a988815272c81554189151f95d2bde497a65e50e
esp0xdeadbeef <esp0xdeadbeef@gmail.com>
2026-05-02 22:08:51 +0200

    Refresh router validation IPv6 runtime values and encrypted secret fields

--- nixos/virtual-machine/nixos-anywhere/s-router-hetzner-anywhere/runtime.nix
+++ nixos/virtual-machine/nixos-anywhere/s-router-hetzner-anywhere/runtime.nix
@@ -1,13 +1,13 @@
 {
   publicIPv4 = "62.238.15.116";
-  publicIPv6 = "2a01:4f9:c014:c422::/64";
-  publicIPv6Address = "2a01:4f9:c014:c422::1/64";
+  publicIPv6 = "2a01:4f9:c012:7695::/64";
+  publicIPv6Address = "2a01:4f9:c012:7695::1/64";
   floatingIPv6Prefixes = [
-    "2a01:4f9:c01f:77::/64"
+    "2a01:4f9:c01f:18::/64"
   ];
   authorizedKeys = [
     "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIAqEmMbztRhj2zE1dXf5Z+Ow7mXXXE6sNAG4/hrIOrmD deadbeef@codex-jail"
-    "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIPHkJNoMlED+bhQMQIKTBb7g+dkq4WANo34iWHkX9LaE root@s-router-test"
+    "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIKLLxXh18rihWnA/X7HMzm7dMW8eImnpWKfXZm6h1+ne root@s-router-test"
   ];
   primaryInterface = "ens3";
   primaryInterfaceMatch = "en* eth0";
