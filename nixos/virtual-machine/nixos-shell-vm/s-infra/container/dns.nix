72d235a82ef4bdc6456c704b19b5e6d789512e01
esp0xdeadbeef <esp0xdeadbeef@gmail.com>
2026-05-07 22:53:23 +0200

    Update s-infra container DNS settings for routed resolver behavior changes

--- nixos/virtual-machine/nixos-shell-vm/s-infra/container/dns.nix
+++ nixos/virtual-machine/nixos-shell-vm/s-infra/container/dns.nix
@@ -3,14 +3,6 @@
   #services.resolved.enable = false;
   services.resolved.enable = true;
 
-  networking.nameservers = [
-    "1.1.1.1"
-    "8.8.8.8"
-    "2606:4700:4700::1111"
-    "2606:4700:4700::1001"
-    "2606:4700:4700::1001"
-    "2001:4860:4860::8888"
-    "2001:4860:4860::8844"
-  ];
+  networking.nameservers = [ ];
   networking.useHostResolvConf = lib.mkForce false;
 }
