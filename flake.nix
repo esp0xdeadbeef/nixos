2a2ae7a7f08e512357759a55a1a40b0377a68cd1
esp0xdeadbeef <esp0xdeadbeef@gmail.com>
2026-06-05 22:43:27 +0200

    updated homemanager also to 26.05 (fix typo)

--- flake.nix
+++ flake.nix
@@ -89,7 +89,7 @@
     #};
 
     home-manager = {
-      url = "github:nix-community/home-manager/release-26.06";
+      url = "github:nix-community/home-manager/release-26.05";
       inputs.nixpkgs.follows = "nixpkgs";
     };
 
