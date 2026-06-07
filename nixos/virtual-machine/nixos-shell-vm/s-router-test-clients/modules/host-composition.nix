e26993bf2c7f184641a8c23ba18a21bccaa8f7b7
esp0xdeadbeef <esp0xdeadbeef@gmail.com>
2026-06-07 17:09:29 +0200

    used double define for clab and nixos

--- nixos/virtual-machine/nixos-shell-vm/s-router-test-clients/modules/host-composition.nix
+++ nixos/virtual-machine/nixos-shell-vm/s-router-test-clients/modules/host-composition.nix
@@ -13,7 +13,18 @@ in
   imports = [
     (cpm.clientFixtures.hostModuleFromPaths {
       intentPath = "${labPath}/intent.nix";
-      inventoryPath = "${labPath}/inventory.nix";
+      inventoryPath = "${labPath}/inventory-nixos.nix";
+      sopsPath = "${labPath}/sops.nix";
+
+      fixture = {
+        kind = "emulated-clients";
+        hostName = "s-router-test-clients";
+        siteName = "nixos";
+      };
+    })
+    (cpm.clientFixtures.hostModuleFromPaths {
+      intentPath = "${labPath}/intent.nix";
+      inventoryPath = "${labPath}/inventory-clab.nix";
       sopsPath = "${labPath}/sops.nix";
 
       fixture = {
