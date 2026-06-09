5b8d25d11eb7d898d56e719cad4dfc52e2bbfcd1
esp0xdeadbeef <esp0xdeadbeef@gmail.com>
2026-06-09 16:11:17 +0200

    remove host-deploy.nix import, deployment owned by CLAB renderer hostModule

--- nixos/virtual-machine/nixos-shell-vm/s-router-clab/default.nix
+++ nixos/virtual-machine/nixos-shell-vm/s-router-clab/default.nix
@@ -4,12 +4,13 @@
 , ...
 }:
 let
-  labSource = "active-lab";
+  labSource = "HAT/emulated-isp-residential-testnet";
 in
 {
   _module.args.sRouterClabLabProfile = {
     inherit labSource;
     labSelector = "s-router-clab";
+    deploymentHost = "s-router-clab";
   };
 
   networking.hostName = lib.mkForce "s-router-clab";
@@ -17,6 +18,7 @@ in
   imports = [
     "${outPath}/library/10-vms/nixos-shell-vm/host-config-routers-without-network"
     ./management-vlan2.nix
+    ./host-adapter-guard.nix
 
     (import ./renderers.nix {
       inherit inputs lib;
