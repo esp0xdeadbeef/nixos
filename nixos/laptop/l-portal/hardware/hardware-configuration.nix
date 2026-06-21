b7f74af4f5111b3ddf3db279ab97d2455e6d6114
esp0xdeadbeef <esp0xdeadbeef@gmail.com>
2026-06-21 02:35:57 +0200

    Add l-portal disk formatting tools

--- nixos/laptop/l-portal/hardware/hardware-configuration.nix
+++ nixos/laptop/l-portal/hardware/hardware-configuration.nix
@@ -62,6 +62,10 @@
   services.openssh.enable = true;
   environment.systemPackages = with pkgs; [
     btrfs-progs
+    dosfstools
+    e2fsprogs
+    exfatprogs
+    parted
     util-linux
   ];
 }
