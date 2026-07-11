f203f14ae1f4bd3bad970b1362e515d606fcb4b5
esp0xdeadbeef <esp0xdeadbeef@gmail.com>
2026-07-11 03:17:45 +0200

    chore(s-gamma): update webpage source pin

--- nixos/virtual-machine/nixos-anywhere/s-gamma/webpage-source.nix
+++ nixos/virtual-machine/nixos-anywhere/s-gamma/webpage-source.nix
@@ -4,6 +4,6 @@ builtins.fetchTree {
   type = "github";
   owner = "esp0xdeadbeef";
   repo = "Webpage";
-  rev = "6bec1fd9c5413649c205bab1534b15d55f1dfe44";
-  narHash = "sha256-DJo16Mxq5AaVx2pwaib2ePVa9UrmoxukZlA69KP4fqE=";
+  rev = "19d7551b8243847977c056fa6ba9c081745ba4da";
+  narHash = "sha256-E+VuNHbaaHnnuZb/bX8GFPL+e5yXyoHzChHzIiO++8g=";
 }
