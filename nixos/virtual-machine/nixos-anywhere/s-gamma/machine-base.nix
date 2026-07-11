816ab1d18eb9f5239b6d5576fcfa67bc93355175
esp0xdeadbeef <esp0xdeadbeef@gmail.com>
2026-07-11 03:08:48 +0200

    fix(s-gamma): use github token for webpage source

--- nixos/virtual-machine/nixos-anywhere/s-gamma/machine-base.nix
+++ nixos/virtual-machine/nixos-anywhere/s-gamma/machine-base.nix
@@ -6,6 +6,7 @@
 , ...
 }:
 let
+  githubTokenPath = "/run/secrets/gh-token";
   keyFor = host: lib.fileContents "${outPath}/ssh-keys/deadbeef/${host}.pub";
 in
 {
@@ -112,6 +113,46 @@ in
   sops.age.sshKeyPaths = [
     "/persist/etc/ssh/ssh_host_ed25519_key"
   ];
+
+  sops.secrets.gh-token = {
+    sopsFile = "${outPath}/secrets/s-gamma.yaml";
+    owner = "root";
+    group = "root";
+    mode = "0400";
+    path = githubTokenPath;
+  };
+
+  system.activationScripts.writeRootNixGithubAccessToken = {
+    deps = [ "setupSecrets" ];
+    text = ''
+      token_file=${lib.escapeShellArg githubTokenPath}
+      nix_config_dir=/root/.config/nix
+      nix_config="$nix_config_dir/nix.conf"
+
+      if [ -r "$token_file" ]; then
+        token="$(tr -d '\r\n' < "$token_file")"
+
+        if [ -n "$token" ]; then
+          install -d -m 0700 "$nix_config_dir"
+          tmp="$nix_config.tmp"
+
+          if [ -e "$nix_config" ]; then
+            grep -v -E '^(extra-experimental-features|experimental-features|access-tokens)[[:space:]]*=' "$nix_config" > "$tmp" || true
+          else
+            : > "$tmp"
+          fi
+
+          {
+            printf '%s\n' "extra-experimental-features = nix-command flakes"
+            printf 'access-tokens = github.com=%s\n' "$token"
+          } >> "$tmp"
+
+          chmod 0600 "$tmp"
+          mv "$tmp" "$nix_config"
+        fi
+      fi
+    '';
+  };
 
   services.openssh = {
     enable = true;
--- nixos/virtual-machine/nixos-anywhere/s-gamma/webpage-source.nix
+++ nixos/virtual-machine/nixos-anywhere/s-gamma/webpage-source.nix
@@ -1,9 +1,9 @@
 # Host-local private source pin. Keep this out of the root flake inputs so
 # unrelated hosts do not need access to this private repository.
 builtins.fetchTree {
-  type = "git";
-  url = "ssh://git@github.com/esp0xdeadbeef/Webpage.git";
-  ref = "refs/heads/main";
+  type = "github";
+  owner = "esp0xdeadbeef";
+  repo = "Webpage";
   rev = "6bec1fd9c5413649c205bab1534b15d55f1dfe44";
   narHash = "sha256-DJo16Mxq5AaVx2pwaib2ePVa9UrmoxukZlA69KP4fqE=";
 }
