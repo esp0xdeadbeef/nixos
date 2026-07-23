9fe458ea8ce9b6fd937ed5075da922ff6b718bcc
esp0xdeadbeef <esp0xdeadbeef@gmail.com>
2026-07-23 04:32:16 +0200

    fix(hashcat): honor configured cuda capabilities

--- library/01-general/password-cracking/hashcat.nix
+++ library/01-general/password-cracking/hashcat.nix
@@ -1,12 +1,22 @@
-{ config, pkgs, ... }:
+{ config
+, lib
+, pkgs
+, ...
+}:
 
 let
+  cudaCapabilities = config.nixpkgs.config.cudaCapabilities or [ ];
   pkgsCuda = import pkgs.path {
     system = pkgs.stdenv.hostPlatform.system;
-    config = {
-      allowUnfree = true;
-      cudaSupport = true;
-    };
+    config =
+      {
+        allowUnfree = true;
+        cudaSupport = true;
+      }
+      // lib.optionalAttrs (cudaCapabilities != [ ]) {
+        inherit cudaCapabilities;
+        cudaForwardCompat = config.nixpkgs.config.cudaForwardCompat or false;
+      };
   };
 in
 {
