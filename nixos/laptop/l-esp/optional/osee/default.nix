e070f86736ac3d12c9e31138438ffb29e873ecd1
esp0xdeadbeef <esp0xdeadbeef@gmail.com>
2026-08-13 08:34:56 +0200

    feat(l-esp/osee): enable osee module and use binary exploitation profile

--- nixos/laptop/l-esp/optional/osee/default.nix
+++ nixos/laptop/l-esp/optional/osee/default.nix
@@ -1,8 +1,8 @@
-{ pkgs, ... }:
+{ profiles, ... }:
 {
   imports = [
+    profiles.nixos.workstation.binary-exploitation
     ./lxc-osee/bind-to-lxc.nix
     ./lxc-osee/x2go-client.nix
-    ./rev-tooling/ghidra.nix
   ];
 }
