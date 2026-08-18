e555e35036cd4073f15ccba2e0bb9077e263eb61
esp0xdeadbeef <esp0xdeadbeef@gmail.com>
2026-08-18 16:50:34 +0200

    fix(s-tang): drop host bridge localAddress, keep container on 10.2.90.10

--- nixos/virtual-machine/nixos-shell-vm/s-tang/default.nix
+++ nixos/virtual-machine/nixos-shell-vm/s-tang/default.nix
@@ -90,7 +90,6 @@ in
     autoStart = true;
     privateNetwork = true;
     hostBridge = "tang-br";
-    localAddress = "10.2.90.2/24";
 
     bindMounts."/var/lib/tang" = {
       hostPath = "/persist/var/lib/tang";
