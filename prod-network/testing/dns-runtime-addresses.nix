74ccfd5f3c09c3f82bdd9163342c198d458ae835
esp0xdeadbeef <esp0xdeadbeef@gmail.com>
2026-08-19 07:07:04 +0200

    feat(neon): realize plane model access nodes and DNS runtime addresses

--- prod-network/testing/dns-runtime-addresses.nix
+++ prod-network/testing/dns-runtime-addresses.nix
@@ -37,5 +37,53 @@
       clientIpv4 = "192.168.8.0/24";
       clientIpv6 = "fd42:dead:beef:8::/64";
     };
+    access-mgmt = {
+      ipv4 = "10.3.10.1";
+      ipv6 = "fd42:dead:beef:310::1";
+      clientIpv4 = "10.3.10.0/24";
+      clientIpv6 = "fd42:dead:beef:310::/64";
+    };
+
+    access-svc = {
+      ipv4 = "10.3.20.1";
+      ipv6 = "fd42:dead:beef:320::1";
+      clientIpv4 = "10.3.20.0/24";
+      clientIpv6 = "fd42:dead:beef:320::/64";
+    };
+
+    access-clients = {
+      ipv4 = "10.3.30.1";
+      ipv6 = "fd42:dead:beef:330::1";
+      clientIpv4 = "10.3.30.0/24";
+      clientIpv6 = "fd42:dead:beef:330::/64";
+    };
+
+    access-iot = {
+      ipv4 = "10.3.50.1";
+      ipv6 = "fd42:dead:beef:350::1";
+      clientIpv4 = "10.3.50.0/24";
+      clientIpv6 = "fd42:dead:beef:350::/64";
+    };
+
+    access-iot-srv = {
+      ipv4 = "10.3.51.1";
+      ipv6 = "fd42:dead:beef:351::1";
+      clientIpv4 = "10.3.51.0/24";
+      clientIpv6 = "fd42:dead:beef:351::/64";
+    };
+
+    access-dmz = {
+      ipv4 = "10.3.60.1";
+      ipv6 = "fd42:dead:beef:360::1";
+      clientIpv4 = "10.3.60.0/24";
+      clientIpv6 = "fd42:dead:beef:360::/64";
+    };
+
+    access-unlock = {
+      ipv4 = "10.3.90.1";
+      ipv6 = "fd42:dead:beef:390::1";
+      clientIpv4 = "10.3.90.0/24";
+      clientIpv6 = "fd42:dead:beef:390::/64";
+    };
   };
 }
