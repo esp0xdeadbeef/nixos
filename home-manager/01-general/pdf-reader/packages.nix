7d0b2bde862ee79ff897254c15622bf52c1390e2
esp0xdeadbeef <esp0xdeadbeef@gmail.com>
2026-09-12 13:00:14 +0200

    fix(packages): replace removed bloodhound with bloodhound-ce and drop unused koodo-reader

--- home-manager/01-general/pdf-reader/packages.nix
+++ home-manager/01-general/pdf-reader/packages.nix
@@ -8,11 +8,7 @@
     let
       stable = with pkgs; [
         kdePackages.okular
-        # koodo-reader # old version of electron
-      ];
-      unstable = with pkgs.unstable; [
-        koodo-reader # old version of electron
       ];
     in
-    stable ++ unstable;
+    stable;
 }
